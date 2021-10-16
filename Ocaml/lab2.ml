64aa5c2858b8b54ebfe26c83090235a5aac1dcfe
Wiktor Sadowy <wiktor.sadowy@onet.pl>
2021-10-16 17:47:02 +0200

    Add task 2 Ocaml

--- Ocaml/lab2.ml
+++ Ocaml/lab2.ml
@@ -4,10 +4,31 @@ let rec sum list =
   else List.hd list + sum (List.tl list)
 ;;
 
-(*Helper for Task 2*)
+(*Helper for task 2*)
+let rec isSequenceCorrect list =
+  if list = [] then false
+  else if List.tl list = ["."] || List.tl list = ["?"] || List.tl list = ["!"] then true
+  else isSequenceCorrect (List.tl list)
+;;
+
+(*Task 2*)
+let rec concatenateStrings list =
+  let rec appendWords list =
+    if List.hd list = "." || List.hd list = "?" || List.hd list = "!" then List.hd list
+    else " " ^ List.hd list ^ appendWords(List.tl list)
+  in
+
+  if isSequenceCorrect list <> true then raise(Failure "Incorrect sequence")
+  else List.hd list ^ appendWords(List.tl list)
+;;
 
 print_int (sum [1; 3; 6]);;
 print_int (sum [-4; 89; 1; -56]);;
-print_int (sum [])
+print_int (sum []);;
 
-  
+print_string (concatenateStrings ["Hello"; "World"; "!"]);;
+print_string (concatenateStrings ["Did"; "I"; "code"; "this"; "task"; "properly"; "?"]);;
+print_string (concatenateStrings ["You"; "are"; "wonderful"; "."]);;
+(*Will print error*)
+(*concatenateStrings [];;*)
+(*concatenateStrings ["."; "Hello"];;*)
