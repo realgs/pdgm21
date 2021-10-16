1f9b034e1912ea767715eb2689206df33f16719b
Monika <260278@student.pwr.edu.pl>
2021-10-16 16:52:57 +0200

    Added Scala files

---
+++ Lista2.scala
@@ -0,0 +1,32 @@
+object Lista2 {
+
+  def len[A](xs:List[A]):Int=(
+    if xs.isEmpty then 0
+    else 1+len(xs.tail)
+  )
+  
+  //zadanie 1
+  def summarize(xs:List[Int]):Int={
+  if len(xs)==0 then 0
+  else xs.head+summarize(xs.tail)
+  }
+  //zadanie 2
+  def sentence(xs: List[String], x:String):String={
+    if len(xs)==0 then x
+    else if len(xs)==1 then xs.head+x
+    else xs.head+" "+sentence(xs.tail, x)
+  }
+  //zadanie 3
+  def greaterThanZero(xs:List[Int]):Boolean={
+    if len(xs)==0 then true
+    else if xs.head>0 then greaterThanZero(xs.tail)
+    else false
+  }
+  //zadanie 4
+  def factorial(x:Int):Int={
+    if x<0 then throw new Exception("Number has to be greater or equal zero.")
+    else if x==0 then 0
+    else if x==1 then 1
+    else x*factorial(x-1)
+  }
+}
