71a70bf838009ab167a19d7c75d25aad3cd1b4cc
Wiktor Sadowy <wiktor.sadowy@onet.pl>
2021-10-16 18:03:06 +0200

    Finish Ocaml tasks

--- Ocaml/lab2.ml
+++ Ocaml/lab2.ml
@@ -22,6 +22,20 @@ let rec concatenateStrings list =
   else List.hd list ^ appendWords(List.tl list)
 ;;
 
+(*Task 3*)
+let rec isPositive list =
+  if list = [] then true
+  else if List.hd list <= 0 then false
+  else isPositive(List.tl list)
+;;
+
+(*Task 4*)
+let rec calculateFactorial n =
+  if n < 0 then raise(Failure "Cannot calculate negative factorial")
+  else if n = 0 then 1
+  else n * calculateFactorial(n-1)
+;;
+
 print_int (sum [1; 3; 6]);;
 print_int (sum [-4; 89; 1; -56]);;
 print_int (sum []);;
@@ -32,3 +46,13 @@ print_string (concatenateStrings ["You"; "are"; "wonderful"; "."]);;
 (*Will print error*)
 (*concatenateStrings [];;*)
 (*concatenateStrings ["."; "Hello"];;*)
+
+print_string (string_of_bool (isPositive [1; 5; 7]));;
+print_string (string_of_bool (isPositive [5; -2; 3]));;
+print_string (string_of_bool (isPositive []));;
+
+print_int (calculateFactorial(0));;
+print_int (calculateFactorial(1));;
+print_int (calculateFactorial(5));;
+(*Will throw an exception*)
+(*print_int (calculateFactorial(-1));;*)
