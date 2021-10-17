5887abef5b08f630cefb743d1be95e8094a63b7e
desty2k <55806660+desty2k@users.noreply.github.com>
2021-10-17 15:39:48 +0200

    ADD: Ocaml solution for task 2

---
+++ list2/list2.ml
@@ -0,0 +1,41 @@
+let rec sum list =
+    if list = [] then 0 else List.hd list + sum(List.tl list);;
+
+sum([1; 2; 3; 4; 5]);;
+sum([-2; -1; 0; 5; -6; 3]);;
+sum([]);;
+
+
+let spacing = " ";;
+
+let rec join(list, ending) =
+    if list = [] then ""
+    else
+        if List.tl list = [] then List.hd list ^ ending
+        else List.hd list ^ spacing ^ join(List.tl list, ending);;
+
+join(["To"; "nie"; "tak"], "!");;
+join(["Ala"; "ma"; "kota"], "?");;
+join([], "some string");;
+
+
+let rec greater_than_zero list =
+    if list = [] then false
+    else
+        if List.tl list = [] then List.hd list > 0
+        else List.hd list > 0 && greater_than_zero(List.tl list);;
+
+greater_than_zero([1; -5; 3; 2]);;
+greater_than_zero([1; 2; 5; 7; 8]);;
+greater_than_zero([]);;
+
+
+let rec factorial n =
+    if n < 0 then 0
+    else
+        if n = 0 then 1
+        else n * factorial(n - 1);;
+
+factorial(-5);;
+factorial(0);;
+factorial(6);;
