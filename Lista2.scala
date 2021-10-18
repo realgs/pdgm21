81a17864d3d9043904f656a8770d43f92f4c2ca8
Monika <260278@student.pwr.edu.pl>
2021-10-18 20:13:51 +0200

    Polishing

--- Lista2.scala
+++ Lista2.scala
@@ -2,7 +2,7 @@ object Lista2 {
 
   def len[A](xs:List[A]):Int=(
     if xs.isEmpty then 0
-    else 1+len(xs.tail)
+    else 1 + len(xs.tail)
   )
   
   //zadanie 1
@@ -11,15 +11,17 @@ object Lista2 {
   else xs.head+summarize(xs.tail)
   }
   //zadanie 2
+  val separator = " "
+
   def sentence(xs: List[String], x:String):String={
     if xs.isEmpty then x
     else if len(xs)==1 then xs.head+x
-    else xs.head+" "+sentence(xs.tail, x)
+    else xs.head + separator + sentence(xs.tail, x)
   }
   //zadanie 3
   def greaterThanZero(xs:List[Int]):Boolean={
     if xs.isEmpty then true
-    else if xs.head>0 then greaterThanZero(xs.tail)
+    else if xs.head > 0 then greaterThanZero(xs.tail)
     else false
   }
   //zadanie 4
@@ -27,6 +29,6 @@ object Lista2 {
     if x<0 then throw new Exception("Number has to be greater or equal zero.")
     else if x==0 then 0
     else if x==1 then 1
-    else x*factorial(x-1)
+    else x * factorial(x-1)
   }
 }
--- l2.ml
+++ l2.ml
@@ -1,5 +1,5 @@
 let rec len(x)=(
-    if x=[] then 0 else 1+len(List.tl x));;
+    if x=[] then 0 else 1 + len(List.tl x));;
 
 (*zadanie 1*)
 let rec summarize(xs)=(
@@ -9,15 +9,17 @@ summarize([-5;7;0;3;34]);;
 summarize([]);;
 
 (*zadanie 2*)
+let separator = " ";;
+
 let rec sentence(xs, x)=(
-    if xs=[] then x else if len(xs)=1 then List.hd xs^x else List.hd xs^" "^sentence(List.tl xs,x));;
+    if xs=[] then x else if len(xs) = 1 then List.hd xs^x else List.hd xs ^ separator ^ sentence(List.tl xs,x));;
 sentence(["Some";"sentence"],"?");;
 sentence(["Another"; "sentence";"a";"little";"bit";"longer"],".");;
 sentence([],";");;
 
 (*zadanie 3*)
 let rec greaterThanZero(xs)=(
-    if xs=[] then true else if List.hd xs>0 then greaterThanZero(List.tl xs) else false);;
+    if xs=[] then true else if List.hd xs > 0 then greaterThanZero(List.tl xs) else false);;
 greaterThanZero([1;2;3;4]);;
 greaterThanZero([]);;
 greaterThanZero([1;-12;5]);;
