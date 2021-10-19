a92963e6e3f2414f5a6f7f401f6b8be87ceb6275
elpitero <pio_dud@wp.pl>
2021-10-19 21:05:05 +0200

    Ocalm - list 2

---
+++ l1.ml
@@ -0,0 +1,41 @@
+(*zadanie 1*)
+let rec sum xs =
+  if xs = [] then 0 else
+    List.hd xs + sum (List.tl xs);;
+
+sum [1;2;3;4;5] = 15;;
+sum [] = 0;;
+sum [-2;-1;0;1;2] = 0;;
+
+
+(*zadanie 2*)
+let rec makeSentence xs =
+  if xs = [] then "" else
+    if List.tl xs = [] then List.hd xs else
+      if List.tl (List.tl xs) = [] then List.hd xs ^  makeSentence (List.tl xs) else
+        List.hd xs ^ " " ^ makeSentence (List.tl xs);;
+
+makeSentence ["Ala";"ma";"kota";"."] = "Ala ma kota.";;
+makeSentence [] = "";;
+makeSentence ["Hi";"!"] = "Hi!";;
+
+
+(*zadanie 3*)
+let rec isGreaterThan0 xs =
+  if xs = [] then true else
+    if List.hd xs < 0 then false else
+      isGreaterThan0 (List.tl xs);;
+
+isGreaterThan0 [] = true;;
+isGreaterThan0 [1;2;3;4;5;6;7] = true;;
+isGreaterThan0 [1;2;3;-1;4;5] = false;;
+
+
+(*zadanie 4*)
+let rec factorial n =
+  if n = 0 then 1 else
+    n * factorial (n - 1);;
+
+factorial 0 = 1;;
+factorial 1 = 1;;
+factorial 6 = 720;;
