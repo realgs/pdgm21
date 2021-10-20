1799133983f24846baefdd6e612950f90f9dfb1f
filipmaciejak <fimaciejak@gmail.com>
2021-10-20 19:17:23 +0200

    task 2 in ocaml

--- l2.ml
+++ l2.ml
@@ -5,3 +5,12 @@ let rec sum xs =
 sum([1;2;3;4]) = 10;;
 sum([]) = 0;;
 sum([1;-1;2]) = 2;;
+
+let rec makeSentence (xs, x) =
+    if xs = [] then x
+    else if List.tl xs = [] then (List.hd xs) ^ x
+    else (List.hd xs) ^ " " ^ makeSentence (List.tl xs, x);;
+
+makeSentence (["Ala";"ma";"kota"], ".") = "Ala ma kota.";;
+makeSentence (["ya";"like";"jazz"], "?") = "ya like jazz?";;
+makeSentence ([], "!") = "!";;
