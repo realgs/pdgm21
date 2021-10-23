fc052b6b9011998fc2da0401cba4d16ccf5b6d6a
Rafał <rafalkruszyna10@gmail.com>
2021-10-23 21:43:24 +0200

    Add test

--- Lab3/Lab3.ml
+++ Lab3/Lab3.ml
@@ -12,4 +12,10 @@ let rec joinLists (list1, list2) =
     ([],[]) -> []
    |([],head2 :: tail2) -> head2::joinLists (list1, tail2)
    |(head1 :: tail1,[]) -> head1::joinLists (tail1, list2)
-   | (head1 :: tail1, head2 :: tail2) -> head1::head2::joinLists (tail1,tail2);;
+   | (head1 :: tail1, head2 :: tail2) -> head1::head2::joinLists (tail1, tail2);;
+
+
+joinLists([1; 2; 3; 4],[1; 2; 3; 4]) = [1; 1; 2; 2; 3; 3; 4; 4];;
+joinLists([],[]) = [];;
+joinLists([1; 2],[4; 5 ; 6]) = [1; 4; 2; 5; 6]
+
