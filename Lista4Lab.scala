b775a26df42bfd281aa791ac2f8ef1194a666d1d
cisco18 <hubertijacek@wp.pl>
2021-11-04 21:09:06 +0100

    Added Tail recursion to exercise 1 and more tests

--- Lista4Lab.scala
+++ Lista4Lab.scala
@@ -35,6 +35,40 @@ object Lista4Lab {
 
   }
 
+  def substringListTail(list1: List[String], listOfElements: List[String]): List[String] = {
+
+    def matchSubstring(partOfList: String, element: String, keyWord: String): String = {
+      if (partOfList == "") {
+        ""
+      } else if (element == "") {
+        keyWord
+      } else {
+        if (partOfList.head == element.head) {
+          matchSubstring(partOfList.tail, element.tail, keyWord);
+        } else
+          matchSubstring(partOfList.tail, element, keyWord);
+      }
+    }
+
+    def matchListsTail(list1: List[String], listOfElements: List[String], acc: List[String]): List[String] = {
+      (list1, listOfElements) match {
+        case (list1, h :: t) =>  matchListsTail(list1, t,acc:::matchElementsTail(list1, h, Nil)  )
+        case (list1, Nil) => acc
+      }
+    }
+
+    def matchElementsTail(list1: List[String], element: String, acc: List[String]): List[String] = {
+      (list1, element) match {
+        case (Nil, element) => acc
+        case (h :: t, element) => matchElementsTail(t, element, matchSubstring(h,element,h) :: acc)
+      }
+    }
+
+    matchListsTail(list1, listOfElements, Nil).filter(_ != "");
+
+
+  }
+
 
 
 
@@ -78,8 +112,18 @@ def substringList2(list1: List[String], elemList: List[String]): List[String] = 
 
   def main(args: Array[String]): Unit = {
     println(addLists(List(1, 2, 3), List(4, 5, 6), List(7, 8, 9)));
+    println(addLists(List(2, 2, 3), List(4), List(7, 8, 9,11, 2)));
+    println(addLists(List(1, 2, 3), Nil, List(7, 8, 9)));
+    println(addLists(List(1, 2, 3), List(4, 5, 6), Nil));
+    println(addLists(Nil, List(4, 5, 6), List(7, 8, 9)));
     println(substringList(List("ADA1Masd", "SADdsa3SD", "ADaSA", "hubaobfs"), List("ADA", "SAD")));
-
+    println(substringList(List("zygmSDASxs", "asfasftssafsfa", "ddddtrszygmsads"," aasdasdsads"), List("zygm", "tss")));
+    println(substringList(List("yyyysysds", "bbcccbbccc", "taaaaaaaaasdsa", ""), List("aaa", "bbb")));
+    println(substringList(List(), List("ADA", "SAD")));
+    println(substringListTail(List("ADA1Masd", "SADdsa3SD", "ADaSA", "hubaobfs"), List("ADA", "SAD")));
+    println(substringListTail(List("zygmSDASxs", "asfasftssafsfa", "ddddtrszygmsads"," aasdasdsads"), List("zygm", "tss")));
+    println(substringListTail(List("yyyysysds", "bbcccbbccc", "taaaaaaaaasdsa", ""), List("aaa", "bbb")));
+    println(substringListTail(List(), List("ADA", "SAD")));
   }
 
 }
