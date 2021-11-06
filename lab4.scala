23e9a9ff33c052fc0f7c23cbf750716b4eecdb0a
NeverGiveUPek <pdruczkin@gmail.com>
2021-11-06 17:54:01 +0100

    1 exercise solution

--- lab4.scala
+++ lab4.scala
@@ -2,40 +2,60 @@ import annotation.tailrec
 
 object Main {
   def main(args: Array[String]): Unit = {
-    println(find(List("index0169","iindex0168202","iindex0168211","iindex0168210","iindex0169222","index0169224"),"index0168"))
-    println(find2(List("index0169","iindex0168202","iindex0168211","iindex0168210","iindex0169222","index0169224"),"22","index0168"))
-    println(find3(List("index0169","iindex0168202","iindex0168211","iindex0168210","iindex0169222","index0169224"),"22","index0168"))
-    println(find4(List("index0169","iindex0168202","iindex0168211","iindex0168210","iindex0169222","index0169224"),"22","index0168"))
+    println(find3(List("index0169","iindex0168202","iindex0168211","iindex0168210","iindex0169222","index0169224"),"index0168","22"))
+    println(find4(List("index0169","iindex0168202","iindex0168211","iindex0168210","iindex0169222","index0169224"),"index0168","22"))
 
   }
 }
 
 
-def find(list: List[String],value : String) =
-  list.filter(x => x.contains(value))
+//O(n) - everyletter check
+def startsWithPhrase(word: String, phrase: String) = {
+  if(phrase == "") then false
+  def startsWithHelper(wordInner: String, phraseInner: String): Boolean =
+    //println(wordInner + " " + phraseInner)
+    (wordInner, phraseInner) match
+      case (_, "") => true
+      case ("", _) => false
+      case (word, phrase) =>  if word.head == phrase.head then startsWithHelper(word.tail, phrase.tail)
+                              else false
+  startsWithHelper(word,phrase)
+}
+//O(n^2) - words letters
+def containsPhrase(word: String, phrase: String) = {
+  if(phrase == "") then false
+  def containsPhraseHelper(innerWord: String):Boolean =
+    //println(innerWord + " " + phrase)
+    if(innerWord == "") then false
+    else if(startsWithPhrase(innerWord,phrase)) then true
+    else containsPhraseHelper(innerWord.tail)
+
+  containsPhraseHelper(word)
+}
 
 
-def find2(list: List[String],values : String*) =
-  list.filter(x => findInMultipleValues(x,values*))
-
+//O(n^3) - values
 def findInMultipleValues(word: String, values : String*):Boolean =
   if values == Nil then false
-  else if word.contains(values.head) then true
+  else if containsPhrase(word,values.head) then true
   else findInMultipleValues(word,values.tail*)
 
 
 def find3(list: List[String],values : String*):List[String] =
-  list match {
+  list match
     case Nil => Nil
     case (h::t) => if(findInMultipleValues(h,values*)) then h :: find3(t,values*) else find3(t,values*)
-  }
 
+//O(n^4) - every item in list
 def find4(list: List[String],values : String*) =
   @tailrec
   def find4Helper(innerList: List[String], result: List[String],values : String* ):List[String] =
-    innerList match {
+    innerList match
       case Nil => result
       case h::t=> if(findInMultipleValues(h,values*)) then find4Helper(t,h::result,values*) else find4Helper(t,result,values*)
-    }
+
   find4Helper(list,Nil,values*)
+
+
+
 
