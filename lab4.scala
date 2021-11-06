66ffbb50282c1a2bf35e4c5c5ac171318917052f
NeverGiveUPek <pdruczkin@gmail.com>
2021-11-06 19:13:52 +0100

    Add Exercise 2 and tests

--- lab4.scala
+++ lab4.scala
@@ -2,18 +2,33 @@ import annotation.tailrec
 
 object Main {
   def main(args: Array[String]): Unit = {
-    println(find3(List("index0169","iindex0168202","iindex0168211","iindex0168210","iindex0169222","index0169224"),"index0168","22"))
-    println(find4(List("index0169","iindex0168202","iindex0168211","iindex0168210","iindex0169222","index0169224"),"index0168","22"))
+    println(find(List("index0169","iindex0168202","iindex0168211","iindex0168210","iindex0169222","index0169224"),"index0168","22")
+      == List("iindex0168202","iindex0168211","iindex0168210","iindex0169222","index0169224"))
+    println(findTail(List("index0169","iindex0168202","iindex0168211","iindex0168210","iindex0169222","index0169224"),"index0168","22")
+      == List("iindex0168202","iindex0168211","iindex0168210","iindex0169222","index0169224"))
 
+    println(find(Nil,"xxx") == Nil)
+    println(findTail(Nil,"xxx") == Nil)
+
+    println(find(List("index0169","iindex0168202","iindex0168211"),"") == Nil)
+    println(findTail(List("index0169","iindex0168202","iindex0168211"),"") == Nil)
+
+    println(joinLists(List(5,4,3,2),List(1,0),List(9)) == List(5,4,3,2,1,0,9))
+    println(joinListsTail(List(5,4,3,2),List(1,0),List(9)) == List(5,4,3,2,1,0,9))
+
+    println(joinLists(List(),List(),List(5,4,3,2)) == List(5,4,3,2))
+    println(joinListsTail(List(),List(),List(5,4,3,2)) == List(5,4,3,2))
+
+    println(joinLists(List("A","B"),List(),List("C")) == List("A","B","C"))
+    println(joinListsTail(List("A","B"),List(),List("C")) == List("A","B","C"))
   }
 }
 
 
 //O(n) - everyletter check
-def startsWithPhrase(word: String, phrase: String) = {
-  if(phrase == "") then false
+def startsWithPhrase(word: String, phrase: String):Boolean = {
+  if(phrase == "") then return false
   def startsWithHelper(wordInner: String, phraseInner: String): Boolean =
-    //println(wordInner + " " + phraseInner)
     (wordInner, phraseInner) match
       case (_, "") => true
       case ("", _) => false
@@ -22,13 +37,13 @@ def startsWithPhrase(word: String, phrase: String) = {
   startsWithHelper(word,phrase)
 }
 //O(n^2) - words letters
-def containsPhrase(word: String, phrase: String) = {
-  if(phrase == "") then false
+def containsPhrase(word: String, phrase: String):Boolean = {
+  if(phrase == "") then return false
   def containsPhraseHelper(innerWord: String):Boolean =
     //println(innerWord + " " + phrase)
     if(innerWord == "") then false
     else if(startsWithPhrase(innerWord,phrase)) then true
-    else containsPhraseHelper(innerWord.tail)
+     else containsPhraseHelper(innerWord.tail)
 
   containsPhraseHelper(word)
 }
@@ -40,21 +55,54 @@ def findInMultipleValues(word: String, values : String*):Boolean =
   else if containsPhrase(word,values.head) then true
   else findInMultipleValues(word,values.tail*)
 
-
-def find3(list: List[String],values : String*):List[String] =
+//O(n^4) - every item in list
+def find(list: List[String],values : String*):List[String] =
   list match
     case Nil => Nil
-    case (h::t) => if(findInMultipleValues(h,values*)) then h :: find3(t,values*) else find3(t,values*)
+    case (h::t) => if(findInMultipleValues(h,values*)) then h :: find(t,values*) else find(t,values*)
 
 //O(n^4) - every item in list
-def find4(list: List[String],values : String*) =
+def findTail(list: List[String],values : String*) =
   @tailrec
-  def find4Helper(innerList: List[String], result: List[String],values : String* ):List[String] =
+  def findHelper(innerList: List[String], result: List[String],values : String* ):List[String] =
     innerList match
       case Nil => result
-      case h::t=> if(findInMultipleValues(h,values*)) then find4Helper(t,h::result,values*) else find4Helper(t,result,values*)
+      case h::t=> if(findInMultipleValues(h,values*)) then findHelper(t,h::result,values*) else findHelper(t,result,values*)
 
-  find4Helper(list,Nil,values*)
+  reverseList(findHelper(list,Nil,values*))
+
+
+
+//2
+//O(n+m+k)
+def joinLists[A](first :List[A], second :List[A], third :List[A]):List[A] =
+  (first,second,third) match
+    case (h::t,_,_) => h::joinLists(t,second,third)
+    case (Nil,h::t,_) => h::joinLists(Nil,t,third)
+    case (Nil,Nil,h::t) => h::joinLists(Nil,Nil,t)
+    case (Nil,Nil,Nil) => Nil
+
+//O(2*(n+m+k)
+def joinListsTail[A](first :List[A], second :List[A], third :List[A]) =
+  @tailrec
+  def joinListsHelper[A](first :List[A], second :List[A], third :List[A], result :List[A]):List[A] =
+    (first,second,third) match
+      case (h::t,_,_) => joinListsHelper(t,second,third,h::result)
+      case (Nil,h::t,_) => joinListsHelper(Nil,t,third,h::result)
+      case (Nil,Nil,h::t) => joinListsHelper(Nil,Nil,t,h::result)
+      case (Nil,Nil,Nil) => reverseList(result)
+  joinListsHelper(first,second,third,Nil)
+
+//O(n)
+def reverseList[A](list :List[A]) = {
+  @tailrec
+  def reverseListHelper[A](srcList :List[A], destList :List[A]):List[A] =
+    if srcList == Nil then destList
+    else reverseListHelper(srcList.tail, srcList.head::destList)
+
+
+  reverseListHelper(list,Nil)
+}
 
 
 
