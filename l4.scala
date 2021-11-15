473c6f3c2aa33f4e44fb2a803b140da87d382258
Jan Eliasz <johnbg@tlen.pl>
2021-11-15 20:25:45 +0100

    Refactor task 1

--- l4.scala
+++ l4.scala
@@ -4,24 +4,15 @@ object l4 {
   def main(args: Array[String]): Unit = {}
 
   // 1
-  def isSubstring(s1: String, s2: String): Boolean =
-    (s1, s2) match
-      case (_, "") => true
-      case ("", _) => false
-      case (s1, s2) => if s1.head == s2.head then (s1.tail, s2.tail) match {
-                                                    case (_, "") => true
-                                                    case ("", _) => false
-                                                    case (t1, t2) => t1.head == t2.head && isSubstring(t1, t2) || t1.head == s1.head && isSubstring(s1.tail, s2)
-                                                  }
-                       else isSubstring(s1.tail, s2)
-
-  def find(list: List[String], elems: List[String]): List[String] =
-    def findHelper(list: List[String], elems: List[String], original: List[String]): List[String] =
-      (list, elems) match
-        case (h1 :: t1, h2 :: t2) if isSubstring(h1, h2) => h1 :: findHelper(t1, original, original)
-        case (h1 :: t1, h2 :: t2) => if t2 == Nil then findHelper(t1, original, original) else findHelper(list, t2, original)
-        case _ => Nil
-    findHelper(list, elems, elems)
+  def subStr(originalSearched: String, originalWwanted: String): Boolean =
+    def subStrHelper(searched: String, wanted: String, nextToBeChecked: String): Boolean =
+      (searched, wanted) match
+        case (_, "") => true
+        case ("", _) => false
+        case (searched, wanted) => if searched.head == wanted.head then subStrHelper(searched.tail, wanted.tail, (if nextToBeChecked != "" then nextToBeChecked else searched.tail))
+                                   else if nextToBeChecked != "" then subStrHelper(nextToBeChecked, originalWwanted, "")
+                                   else subStrHelper(searched.tail, wanted, "")
+    subStrHelper(originalSearched, originalWwanted, "")
 
   def reverseList[A](list: List[A]): List[A] =
     def reverseListHelper[A](acc: List[A], l: List[A]): List[A] =
@@ -29,12 +20,20 @@ object l4 {
       else reverseListHelper(l.head::acc, l.tail)
     reverseListHelper(Nil, list)
 
-  def findT(list: List[String], elems: List[String]): List[String] =
+  def find(indexes: List[String], originalMatchers: List[String]): List[String] =
+    def findHelper(indexes: List[String], matchers: List[String]): List[String] =
+      (indexes, matchers) match
+        case (indexesHead :: indexesTail, matchersHead :: matchersTail) if subStr(indexesHead, matchersHead) => indexesHead :: findHelper(indexesTail, originalMatchers)
+        case (indexesHead :: indexesTail, matchersHead :: matchersTail) => if matchersTail == Nil then findHelper(indexesTail, originalMatchers) else findHelper(indexes, matchersTail)
+        case _ => Nil
+    findHelper(indexes, originalMatchers)
+
+  def findT(indexes: List[String], originalMatchers: List[String]): List[String] =
     @tailrec
-    def findTHelper(list: List[String], elems: List[String], original: List[String], result: List[String]): List[String] =
-      (list, elems) match
-        case (h1 :: t1, h2 :: t2) if isSubstring(h1, h2) => findTHelper(t1, original, original, h1 :: result)
-        case (h1 :: t1, h2 :: t2) => if t2 == Nil then findTHelper(t1, original, original, result) else findTHelper(list, t2, original, result)
+    def findTHelper(indexes: List[String], matchers: List[String], result: List[String]): List[String] =
+      (indexes, matchers) match
+        case (indexesHead :: indexesTail, matchersHead :: matchersTail) if subStr(indexesHead, matchersHead) => findTHelper(indexesTail, originalMatchers, indexesHead :: result)
+        case (indexesHead :: indexesTail, matchersHead :: matchersTail) => if matchersTail == Nil then findTHelper(indexesTail, originalMatchers, result) else findTHelper(indexes, matchersTail, result)
         case _ => reverseList(result)
-    findTHelper(list, elems, elems, Nil)
+    findTHelper(indexes, originalMatchers, Nil)
 }
