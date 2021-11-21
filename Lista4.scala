0178ac61c9879ca45b0eb7a3c7d64f6ebf6a1e34
Agre1n <michalsokolowski12@gmail.com>
2021-11-21 13:58:29 +0100

    Add rewrited Lista4.scala

--- Lista4.scala
+++ Lista4.scala
@@ -6,70 +6,116 @@ object Lista4 {
     @tailrec
     def tailrecHelper (list: List[A], result: List[A]): List[A] = {
       list match
-        case h::t => tailrecHelper(t, h::result)
         case Nil => result
+        case h::t => tailrecHelper(t, h::result)
     }
     tailrecHelper(list, Nil)
   }
+  /*
+  n -> długość listy
+
+  złożoność obliczeniowa: n
+
+  złożoność pamięciowa: n
+  ponieważ stosujemy rekursje ogonową, będziemy potrzebować jedynie miejsca na liste wynikową
+  */
 
   //zadanie 1
 
-  def containsPhrase (word :String, phrase: String): Boolean = {
-    (word, phrase) match
-      case (_, "") => true
-      case ("", _) => false
-      case (w, p) => if w.head == p.head then containsPhrase(w.tail, p.tail) else containsPhrase(w.tail, phrase)
+  def ifTextContainsPhrase(searchText: String, searchedPhrase: String): Boolean = {
+    def helper(text: String, phrase: String): Boolean = {
+      (text, phrase) match
+        case (_, "") => true
+        case ("", _) => false
+        case (t, p) => if t.head != p.head then false else helper(t.tail, p.tail)
+    }
+    searchText match
+      case "" => false
+      case text => if helper(searchText, searchedPhrase) then true
+                    else ifTextContainsPhrase(text.tail, searchedPhrase)
   }
+  /*
+  n - ilość liter "searchText", m - ilość liter "searchedPhrase"
 
-  def find (words: List[String], phrase: String): List[String] = {
-    words match
+  złożoność obliczeniowa: zależna od tego czy tekst zawiera szukaną frazę i w którym miejscu
+                          pesymistycznie - wyszukiwana fraza jest dłuższa od tekstu
+                                          (n + 1) * n/2 = 1/2 n^2 + 1/2 n
+  złożoność pamięciowa: 1
+  ponieważ stosujemy rekursje ogonową, a wynikiem jest Boolean
+  */
+
+  def singleTextManyPhrases(searchText: String, searchedPhrases: List[String]): Boolean = {
+    searchedPhrases match
+      case Nil => false
+      case ""::t => singleTextManyPhrases(searchText, t)
+      case h::t => if ifTextContainsPhrase(searchText, h) then true else singleTextManyPhrases(searchText, t)
+  }
+  /*
+  n - ilość elementów "searchedPhrase"
+
+  złożoność obliczeniowa: zależna od tego czy tekst zawiera którąś z fraz oraz którą
+                          pesymisztycznie - tekst nie zawiera żadnej frazy
+                          n * złożoność "ifTextContainsPhrase" (zależne od długości teksu i poszczególnych fraz)
+
+  złożoność pamięciowa: 1
+  ponieważ stosujemy rekursje ogonową, a wynikiem jest Boolean
+  */
+
+  def searchTextsLookingForPhrases(searchTexts: List[String], searchedPhrases: List[String]): List[String] = {
+    searchTexts match
       case Nil => Nil
-      case h::t => if containsPhrase(h, phrase) then h::find(t, phrase) else find(t, phrase)
+      case h::t => if singleTextManyPhrases(h, searchedPhrases) then h::searchTextsLookingForPhrases(t, searchedPhrases)
+                    else searchTextsLookingForPhrases(t, searchedPhrases)
   }
+  /*
+  n - ilość elementów "searchTexts", m - ilość elementów "searchedPhrases"
 
-  def findTailrec (words: List[String], phrase: String) = {
+  złożoność obliczeniowa: n * złożoność "singleTextManyPhrases" = n * m * złożoność "ifTextContainsPhrase" (zależne od długości poszczególnych teksów i fraz)
+
+  złożoność pamięciowa: n + (n^2 + n)/2 = 1/2 n^2 + 3/2 n
+  ponieważ musimy zapamiętać "n" glów (n) oraz "n" ogonów ((n^2 + n)/2)
+  */
+
+  def searchTextsLookingForPhrasesTailrec(searchTexts: List[String], searchedPhrases: List[String]): List[String] = {
     @tailrec
-    def tailrecHelper (words: List[String], result: List[String]): List[String] = {
-      words match
+    def helper(texts: List[String], phrases: List[String], result: List[String]): List[String] = {
+      texts match
         case Nil => result
-        case h::t => if containsPhrase(h, phrase) then tailrecHelper(t, h::result) else tailrecHelper(t, result)
+        case h::t => if singleTextManyPhrases(h, searchedPhrases) then helper(t, searchedPhrases, h::result)
+                      else helper(t, searchedPhrases, result)
     }
-    reverseList(tailrecHelper(words, Nil))
+    reverseList(helper(searchTexts, searchedPhrases, Nil))
   }
+  /*
+  n - ilość elementów "searchTexts", m - ilość elementów "searchedPhrases", r - ilość elementów w liście wynikowej "result" 0 < r < n
 
-  def findN (words: List[String], phrases: List[String]) = {
-    def recHelper (words:List[String], phrasesLeft: List[String]): List[String] = {
-      (words, phrasesLeft) match
-        case (Nil, _) => Nil
-        case (h::t, Nil) => recHelper(t, phrases)
-        case (h1::t1, h2::t2) => if containsPhrase(h1, h2) then h1::recHelper(t1, phrases) else recHelper(words, t2)
-    }
-    recHelper(words, phrases)
-  }
+  złożoność obliczeniowa: n * złożoność "singleTextManyPhrases" + złożoność "reverseList" =
+                          = n * m * złożoność "ifTextContainsPhrase" (zależne od długości poszczególnych teksów i fraz) + r
 
-  def findNTailrec (words: List[String], phrases: List[String]) = {
-    @tailrec
-    def tailrecHelper (words:List[String], phrasesLeft: List[String], result: List[String]): List[String] = {
-      (words, phrasesLeft) match
-        case (Nil, _) => result
-        case (h::t, Nil) => tailrecHelper(t, phrases, result)
-        case (h1::t1, h2::t2) => if containsPhrase(h1, h2) then tailrecHelper(t1, phrases, h1::result) else tailrecHelper(words, t2, result)
-    }
-    reverseList(tailrecHelper(words, phrases, Nil))
-  }
+  złożoność pamięciowa r + złożoność "reverseList" = 2 r
+  ponieważ stosujemy rekursje ogonową, będziemy potrzebować jedynie miejsca na liste wynikową
+  */
 
   //zadanie 2
 
-  def joinLists [A](list1: List[A], list2: List[A], list3: List[A]): List[A] = {
+  def joinLists[A](list1: List[A], list2: List[A], list3: List[A]): List[A] = {
     (list1, list2) match
       case (h::t, _) => h::joinLists(t, list2, list3)
       case (Nil, h::t) => h::joinLists(Nil, t, list3)
       case (Nil, Nil) => list3
   }
+  /*
+  n -> długość pierwszej listy, m -> długość drugiej listy
 
-  def joinListsTailrec [A](list1: List[A], list2: List[A], list3: List[A]) = {
+  złożoność obliczeniowa: n + m
+
+  złożoność pamięciowa: n + (n^2 + n)/2 + m + (m^2 + m)/2 = 1/2 (n^2 + m^2) + 3/2 (n + m)
+  ponieważ musimy zapamiętać "n" głów (n), "n" ogonów ((n^2 + n)/2), "m" głów (m) oraz "m" ogonów ((m^2 + m)/2)
+  */
+
+  def joinListsTailrec[A](list1: List[A], list2: List[A], list3: List[A]): List[A] = {
     @tailrec
-    def tailrecHelper (list1: List[A], list2: List[A], list3: List[A], result: List[A]): List[A] = {
+    def tailrecHelper(list1: List[A], list2: List[A], list3: List[A], result: List[A]): List[A] = {
       (list1, list2, list3) match
         case (h::t, _, _) => tailrecHelper(t, list2, list3, h::result)
         case (Nil, h::t, _) => tailrecHelper(Nil, t, list3, h::result)
@@ -78,36 +124,33 @@ object Lista4 {
     }
     reverseList(tailrecHelper(list1, list2, list3, Nil))
   }
+  /*
+  n -> długość pierwszej listy, m -> długość drugiej listy, o -> długość trzeciej listy
+
+  złożoność obliczeniowa: n + m + o + złożoność reverseList = 2(n + m + o)
+
+  złożoność pamięciowa: n + m + o + złożoność reverseList = 2(n + m + o)
+  ponieważ stosujemy rekursje ogonową, będziemy potrzebować jedynie miejsca na liste wynikową
+  */
 
   def main(args: Array[String]): Unit = {
-    //zadanie 1 testy
+    //zadanie 1
 
-    println(find(List("index0169" ,"iindex0168202", "iindex0168211", "iindex0168210", "iindex0169222"), "index0168"))
-    println(find(List(), "a"))
-    println(find(List("zadanie", "drugie"), ""))
+    println(searchTextsLookingForPhrases(List("index0169" ,"iindex0168202", "iindex0168211", "iindex0168210", "iindex0169222","index0168"), List("index0168")))
+    println(searchTextsLookingForPhrases(List("index0169" ,"iindex0168202", "iindex0168211", "iindex0168210", "iindex0169222"), List("index016820", "index016922")))
+    println(searchTextsLookingForPhrases(List(""), List("index0168", "222")))
+    println(searchTextsLookingForPhrases(List("index0169" ,"iindex0168202", "iindex0168211", "iindex0168210", "iindex0169222"), List()))
+    println(searchTextsLookingForPhrases(List("index0169" ,"iindex0168202", "iindex0168211", "iindex0168210", "iindex0169222"), List("")))
     println()
 
-    println(findTailrec(List("index0169" ,"iindex0168202", "iindex0168211", "iindex0168210", "iindex0169222"), "index0168"))
-    println(findTailrec(List(), "a"))
-    println(findTailrec(List("zadanie", "drugie"), ""))
+    println(searchTextsLookingForPhrasesTailrec(List("index0169" ,"iindex0168202", "iindex0168211", "iindex0168210", "iindex0169222","index0168"), List("index0168")))
+    println(searchTextsLookingForPhrasesTailrec(List("index0169" ,"iindex0168202", "iindex0168211", "iindex0168210", "iindex0169222"), List("index016820", "index016922")))
+    println(searchTextsLookingForPhrasesTailrec(List(""), List("index0168", "222")))
+    println(searchTextsLookingForPhrasesTailrec(List("index0169" ,"iindex0168202", "iindex0168211", "iindex0168210", "iindex0169222"), List()))
+    println(searchTextsLookingForPhrasesTailrec(List("index0169" ,"iindex0168202", "iindex0168211", "iindex0168210", "iindex0169222"), List("")))
     println()
 
-    println(findN(List("index0169" ,"iindex0168202", "iindex0168211", "iindex0168210", "iindex0169222"), List("index0168")))
-    println(findN(List("index0169" ,"iindex0168202", "iindex0168211", "iindex0168210", "iindex0169222"), List("index016820", "index016922")))
-    println(findN(List(), List("index0168", "222")))
-    println(findN(List("index0169" ,"iindex0168202", "iindex0168211", "iindex0168210", "iindex0169222"), List()))
-    println(findN(List("index0169" ,"iindex0168202", "iindex0168211", "iindex0168210", "iindex0169222"), List("")))
-    println()
-
-    println(findNTailrec(List("index0169" ,"iindex0168202", "iindex0168211", "iindex0168210", "iindex0169222"), List("index0168")))
-    println(findNTailrec(List("index0169" ,"iindex0168202", "iindex0168211", "iindex0168210", "iindex0169222"), List("index016820", "index016922")))
-    println(findNTailrec(List(), List("index0168", "222")))
-    println(findNTailrec(List("index0169" ,"iindex0168202", "iindex0168211", "iindex0168210", "iindex0169222"), List()))
-    println(findNTailrec(List("index0169" ,"iindex0168202", "iindex0168211", "iindex0168210", "iindex0169222"), List("")))
-    println()
-
-    //zadanie 2 testy
-
+    //zadanie 2
     println(joinLists(List(5, 4, 3, 2), List(1, 0), List(9)))
     println(joinLists(List(), List(), List()))
     println(joinLists(List('m', 'i'), List('c', 'h', 'a'), List('ł')))
