7eaee19459f16faa43519621eaeeab429d4eaee6
Wojciech Begierski <wojtek.begierski@tlen.pl>
2021-11-29 19:22:04 +0100

    fix bugs and trees

--- Lab5.scala
+++ Lab5.scala
@@ -6,6 +6,13 @@ import scala.util.Random
 
 object Main {
 
+  /*
+
+    Poprawione zaczyna się w linijce 103,
+    dodatkowo zmieniłem argument decimalToAnySystem na 2
+
+  */
+
   //Zadanie 1
 
   //Złożoność obliczeniowa O(n/16+1), n - liczba początkowa
@@ -13,26 +20,26 @@ object Main {
   // Zadbałem o złozonosc poprzez uzycie rekurencji ogonowej oraz
   // uzycie :: ktore dolacze elemeny na poczatek listy
   def decimalToHex(number: Int): List[Int] =
-    @scala.annotation.tailrec
-    def decimalToHexHelper(number: Int, resultList: List[Int]): List[Int] =
-      if number == 0 then resultList
-      else decimalToHexHelper( number/16 , number%16 :: resultList)
-    if number > 0 then decimalToHexHelper(number, List())
-    else if number == 0 then List(0)
-    else -1::decimalToHexHelper(-number, List())
+  @tailrec
+  def decimalToHexHelper(number: Int, resultList: List[Int]): List[Int] =
+    if number == 0 then resultList
+    else decimalToHexHelper( number/16 , number%16 :: resultList)
+  if number > 0 then decimalToHexHelper(number, List())
+  else if number == 0 then List(0)
+  else -1::decimalToHexHelper(-number, List())
 
 
-  def decimalToAnySystem(numberAndConvertToSystem: List[Int]): List[Int] =
-    @scala.annotation.tailrec
-    def decToAnySystemHelper(number: Int, systemNumber: Int, resultList: List[Int]) : List[Int] =
-      if number == 0 then resultList
-      else decToAnySystemHelper(number/systemNumber, systemNumber, (number%systemNumber)::resultList)
+  def decimalToAnySystem(number: Int, system: Int): List[Int] =
+  @tailrec
+  def decToAnySystemHelper(number: Int, systemNumber: Int, resultList: List[Int]) : List[Int] =
+    if number == 0 then resultList
+    else decToAnySystemHelper(number/systemNumber, systemNumber, (number%systemNumber)::resultList)
 
-    if numberAndConvertToSystem(1) <= 1 then throw new Exception("Wrong conversion system")
-    else
-      if numberAndConvertToSystem(0) == 0 then List(0)
-      else if numberAndConvertToSystem(0) > 0 then decToAnySystemHelper(numberAndConvertToSystem(0), numberAndConvertToSystem(1), Nil)
-      else -1::decToAnySystemHelper(-numberAndConvertToSystem(0), numberAndConvertToSystem(1), Nil)
+  if system <= 1 then throw new Exception("Wrong conversion system")
+  else
+  if number == 0 then List(0)
+  else if number > 0 then decToAnySystemHelper(number, system, Nil)
+  else -1::decToAnySystemHelper(-number, system, Nil)
 
   //Drzewa
 
@@ -46,31 +53,31 @@ object Main {
   val random = Random();
   def generateTree(depth: Int): BTree[Double] =
 
-    def generateTreeHelper(depth: Int): BTree[Double] =
-      if depth == 1 then Node(random.nextDouble(), Empty, Empty)
-      else Node(random.nextDouble(), generateTreeHelper(depth-1), generateTreeHelper(depth-1))
+  def generateTreeHelper(depth: Int): BTree[Double] =
+    if depth == 1 then Node(random.nextDouble(), Empty, Empty)
+    else Node(random.nextDouble(), generateTreeHelper(depth-1), generateTreeHelper(depth-1))
 
-    if depth <= 0 then Empty
-    else generateTreeHelper(depth)
+  if depth <= 0 then Empty
+  else generateTreeHelper(depth)
 
   // Zadanie 4
   def multiplyTree(tree: BTree[Double]): Double =
-    def multiplyTreeHelper(tree: BTree[Double], result: Double): Double =
-      tree match
-        case Empty => result
-        case Node(value, left, right) => multiplyTreeHelper(left, multiplyTreeHelper(right, value*result))
-    if tree == Empty then throw new IllegalArgumentException("Tree is empty ")
-    else multiplyTreeHelper(tree, 1.0)
+  def multiplyTreeHelper(tree: BTree[Double], result: Double): Double =
+    tree match
+  case Empty => result
+  case Node(value, left, right) => multiplyTreeHelper(left, multiplyTreeHelper(right, value*result))
+  if tree == Empty then throw new IllegalArgumentException("Tree is empty ")
+  else multiplyTreeHelper(tree, 1.0)
 
   //Zadanie 5
 
   //tworzy drzewo z listy
   def generateTreeFromList[A](list: List[A]): BTree[A] =
     list match
-      case Nil => Empty
-      case head::tail =>
-        val tmp = tail.splitAt(tail.length/2)
-        Node(head, generateTreeFromList(tmp._1), generateTreeFromList(tmp._2))
+  case Nil => Empty
+  case head::tail =>
+  val tmp = tail.splitAt(tail.length/2)
+  Node(head, generateTreeFromList(tmp._1), generateTreeFromList(tmp._2))
 
 
   //funkcja do sprawdzenia czy lista zawiera element
@@ -93,29 +100,75 @@ object Main {
 
   */
 
-  def deleteDuplicateDepth[A](tree: BTree[A]): BTree[A] =
-    def generateList[A](stack: List[BTree[A]], elements: List[A]): List[A]=
-      stack match
-        case Nil => Nil
-        case Empty::tail => generateList(tail, elements)
-        case Node(value, left, right)::tail =>
-          if !listContains(elements, value) then value::generateList(left::right::tail, value::elements)
-          else generateList(left::right::tail,elements)
+  // Poprawione dla przykładu który podałem
 
-    generateTreeFromList(generateList(List(tree),List()))
+  sealed trait BTreeFriends[+A]
+  case object EmptyFr extends BTreeFriends[Nothing]
+  case class NodeFr[+A](value: A, listOfFriends: List[BTreeFriends[A]]) extends BTreeFriends[A]
+
+  /*
+    Stworzone drzewo nie-binarne, usuwane są z niego duplikaty,
+    przetwarzane jest na drzewo binarne,
+    będzie lepiej póżniej je przetwarzać (nie-binarne miałoby mały sens),
+    a kolejność w tym wypadku nie bedzie ważna,tylko brak powtórzeń. Jednak
+    w prawdziwej aplikacji, można by było posortować i stworzyć drzewo binarne,
+    ze względu na np wiek.
+
+  */
+
+  //Dla drzew przyjaciół
+  def deleteDuplicateDepth[A](tree: BTreeFriends[A]): BTree[A] =
+  def generateList[A](stack: List[BTreeFriends[A]], elements: List[A]): List[A]=
+    stack match
+  case Nil => Nil
+  case EmptyFr::tail => generateList(tail, elements)
+  case NodeFr(value, listOfFriends)::tail =>
+  if !listContains(elements, value) then value::generateList(listOfFriends:::tail, value::elements)
+  else generateList(tail,elements)
+
+  val list = generateList(List(tree),List())
+  println(list)
+  generateTreeFromList(list)
+
+  //
+  def deleteDuplicatesBreadth[A](tree: BTreeFriends[A]): BTree[A] =
+  def deleteHelper[A](queue: List[BTreeFriends[A]], visited: List[A]): List[A]=
+    queue match
+  case Nil => Nil
+  case EmptyFr::tail => deleteHelper(tail, visited)
+  case NodeFr(value, listOfFriends)::tail =>
+  if listContains(visited, value) then deleteHelper(tail:::listOfFriends, visited)
+  else value::deleteHelper(tail:::listOfFriends, value::visited)
+  if tree != Empty then
+    generateTreeFromList(deleteHelper(List(tree), List()))
+  else Empty
+
+
+
+  // Dla drzew binarnych
+  def deleteDuplicateDepth[A](tree: BTree[A]): BTree[A] =
+  def generateList[A](stack: List[BTree[A]], elements: List[A]): List[A]=
+    stack match
+  case Nil => Nil
+  case Empty::tail => generateList(tail, elements)
+  case Node(value, left, right)::tail =>
+  if !listContains(elements, value) then value::generateList(left::right::tail, value::elements)
+  else generateList(left::right::tail,elements)
+
+  generateTreeFromList(generateList(List(tree),List()))
 
 
   def deleteDuplicatesBreadth[A](tree: BTree[A]): BTree[A] =
-    def deleteHelper[A](queue: List[BTree[A]], visited: List[A]): List[A]=
-      queue match
-        case Nil => Nil
-        case Empty::tail => deleteHelper(tail, visited)
-        case Node(value, left, right)::tail =>
-          if listContains(visited, value) then deleteHelper(tail:::List(left, right), visited)
-          else value::deleteHelper(tail:::List(left, right), value::visited)
-    if tree != Empty then
-      generateTreeFromList(deleteHelper(List(tree), List()))
-    else Empty
+  def deleteHelper[A](queue: List[BTree[A]], visited: List[A]): List[A]=
+    queue match
+  case Nil => Nil
+  case Empty::tail => deleteHelper(tail, visited)
+  case Node(value, left, right)::tail =>
+  if listContains(visited, value) then deleteHelper(tail:::List(left, right), visited)
+  else value::deleteHelper(tail:::List(left, right), value::visited)
+  if tree != Empty then
+    generateTreeFromList(deleteHelper(List(tree), List()))
+  else Empty
 
   /*
     Jest to dokładnie ta sama funkcja co deleteDuplicatesBreadth, jednak
@@ -125,54 +178,54 @@ object Main {
   */
 
   def deleteDuplicatesBreadthWithReplace(tree: BTree[Double]): BTree[Double] =
-    def deleteHelper(queue: List[BTree[Double]], visited: List[Double]): List[Double]=
-      queue match
-        case Nil => Nil
-        case Empty::tail => deleteHelper(tail, visited)
-        case Node(value, left, right)::tail =>
-          if listContains(visited, value) then -1::deleteHelper(tail:::List(left, right), visited)
-          else value::deleteHelper(tail:::List(left, right), value::visited)
+  def deleteHelper(queue: List[BTree[Double]], visited: List[Double]): List[Double]=
+    queue match
+  case Nil => Nil
+  case Empty::tail => deleteHelper(tail, visited)
+  case Node(value, left, right)::tail =>
+  if listContains(visited, value) then -1::deleteHelper(tail:::List(left, right), visited)
+  else value::deleteHelper(tail:::List(left, right), value::visited)
 
-    if tree != Empty then
-      generateTreeFromList(deleteHelper(List(tree), List()))
-    else Empty
+  if tree != Empty then
+    generateTreeFromList(deleteHelper(List(tree), List()))
+  else Empty
 
   def deleteDuplicateDepthWithReplace(tree: BTree[Double]): BTree[Double] =
-    def generateList(stack: List[BTree[Double]], elements: List[Double]): List[Double]=
-      stack match
-        case Nil => Nil
-        case Empty::tail => generateList(tail, elements)
-        case Node(value, left, right)::tail =>
-          if !listContains(elements, value) then value::generateList(left::right::tail, value::elements)
-          else -1::generateList(left::right::tail,elements)
+  def generateList(stack: List[BTree[Double]], elements: List[Double]): List[Double]=
+    stack match
+  case Nil => Nil
+  case Empty::tail => generateList(tail, elements)
+  case Node(value, left, right)::tail =>
+  if !listContains(elements, value) then value::generateList(left::right::tail, value::elements)
+  else -1::generateList(left::right::tail,elements)
 
-    generateTreeFromList(generateList(List(tree),List()))
+  generateTreeFromList(generateList(List(tree),List()))
 
 
   //====================== STARA DEFINICJA ========================================
 
   // Uwaga, to jest stara definicja gdy polecenie miało treść:
-// Napisz funkcję generującą drzewo o głębokości N (liczba poziomów drzewa), zawierającą losowe liczby całkowite. Drzewo pełne.
-// Jednak zostawiłem
+  // Napisz funkcję generującą drzewo o głębokości N (liczba poziomów drzewa), zawierającą losowe liczby całkowite. Drzewo pełne.
+  // Jednak zostawiłem
   def generateTreeOld(depth: Int, maxValue: Int): BTree[Int] =
-    val random = new Random();
+  val random = new Random();
 
-    //funkcja do generowania liczby losowej z zakresu -n do n BEZ 0!
-    def generateRandomNumber(n: Int): Int =
-      val result = 1+random.nextInt(n)
-      if (random.nextInt(2)==0) result * (-1)
-      else result
+  //funkcja do generowania liczby losowej z zakresu -n do n BEZ 0!
+  def generateRandomNumber(n: Int): Int =
+  val result = 1+random.nextInt(n)
+  if (random.nextInt(2)==0) result * (-1)
+  else result
 
-    def generateTreeHelper(depth: Int): BTree[Int] =
-      if depth == 1 then Node(generateRandomNumber(maxValue), Empty, Empty)
-      else Node(generateRandomNumber(maxValue), generateTreeHelper(depth-1), generateTreeHelper(depth-1))
+  def generateTreeHelper(depth: Int): BTree[Int] =
+    if depth == 1 then Node(generateRandomNumber(maxValue), Empty, Empty)
+    else Node(generateRandomNumber(maxValue), generateTreeHelper(depth-1), generateTreeHelper(depth-1))
 
-    if maxValue <= 0 then
-      throw new IllegalArgumentException(" min value can't be less or equal 0")
-    else if depth < 0 then
-      Empty
-    else
-      generateTreeHelper(depth)
+  if maxValue <= 0 then
+  throw new IllegalArgumentException(" min value can't be less or equal 0")
+  else if depth < 0 then
+    Empty
+  else
+    generateTreeHelper(depth)
 
 
 
@@ -180,13 +233,13 @@ object Main {
 
   // To również jest funkcja dla starej definicji drzewa
   def multiplyTreeOld(tree: BTree[Int]): BigInt =
-    def multiplyTreeHelper(tree: BTree[Int]): BigInt=
-      tree match
-        case Node(value, left, right) => value * multiplyTreeHelper(left) * multiplyTreeHelper(right)
-        case Empty => 1
+  def multiplyTreeHelper(tree: BTree[Int]): BigInt=
     tree match
-      case node: Node[Int] => multiplyTreeHelper(tree)
-      case _ => throw new IllegalArgumentException(" tree is empty ")
+  case Node(value, left, right) => value * multiplyTreeHelper(left) * multiplyTreeHelper(right)
+  case Empty => 1
+  tree match
+  case node: Node[Int] => multiplyTreeHelper(tree)
+  case _ => throw new IllegalArgumentException(" tree is empty ")
 
 
   //===================== POWYŻSZE SĄ DO STAREJ DEFINICJI =====================
@@ -207,24 +260,24 @@ object Main {
     println("Zadanie 2")
     println()
     println("testy na system 16")
-    println( decimalToAnySystem(List(31, 16)) == List(1,15))
-    println( decimalToAnySystem(List(-31, 16)) == List(-1, 1, 15) )
-    println( decimalToAnySystem(List(0, 16)) == List(0) )
-    println( decimalToAnySystem(List(100, 16)) == List(6, 4)  )
+    println( decimalToAnySystem(31, 16) == List(1,15))
+    println( decimalToAnySystem(-31, 16) == List(-1, 1, 15) )
+    println( decimalToAnySystem(0, 16) == List(0) )
+    println( decimalToAnySystem(100, 16) == List(6, 4)  )
 
     println()
     println("testy na system 2")
-    println( decimalToAnySystem(List(31, 2))== List(1, 1, 1, 1, 1) )
-    println( decimalToAnySystem(List(-31, 2)) == List(-1, 1, 1, 1, 1, 1) )
-    println( decimalToAnySystem(List(0, 2)) == List(0) )
-    println( decimalToAnySystem(List(100, 2)) == List(1, 1, 0, 0, 1, 0, 0) )
+    println( decimalToAnySystem(31, 2)== List(1, 1, 1, 1, 1) )
+    println( decimalToAnySystem(-31, 2) == List(-1, 1, 1, 1, 1, 1) )
+    println( decimalToAnySystem(0, 2) == List(0) )
+    println( decimalToAnySystem(100, 2) == List(1, 1, 0, 0, 1, 0, 0) )
 
     println()
     println("testy na system 5")
-    println( decimalToAnySystem(List(3196, 5)) == List(1, 0, 0, 2, 4, 1))
-    println( decimalToAnySystem(List(-3196, 5)) == List(-1, 1, 0, 0, 2, 4, 1) )
-    println( decimalToAnySystem(List(0, 5)) == List(0) )
-    println( decimalToAnySystem(List(99, 5)) == List(3, 4, 4) )
+    println( decimalToAnySystem(3196, 5) == List(1, 0, 0, 2, 4, 1))
+    println( decimalToAnySystem(-3196, 5) == List(-1, 1, 0, 0, 2, 4, 1) )
+    println( decimalToAnySystem(0, 5) == List(0) )
+    println( decimalToAnySystem(99, 5) == List(3, 4, 4) )
 
 
     println()
@@ -313,6 +366,27 @@ object Main {
     println("breadth: " + (deleteDuplicatesBreadthWithReplace(treeNew) == Node(2.0,Node(8.0,Node(-1.0,Empty,Node(-1.0,Empty,Empty)),Node(3.0,Empty,Node(1.0,Empty,Empty))),Node(6.0,Node(-1.0,Empty,Node(5.0,Empty,Empty)),Node(-1.0,Empty,Node(7.0,Empty,Empty)))) ))
     println("depth: " + (deleteDuplicateDepthWithReplace(treeNew) == Node(2.0,Node(8.0,Node(-1.0,Empty,Node(-1.0,Empty,Empty)),Node(5.0,Empty,Node(7.0,Empty,Empty))),Node(1.0,Node(3.0,Empty,Node(-1.0,Empty,Empty)),Node(-1.0,Empty,Node(6.0,Empty,Empty))))))
 
+
+    val treeFriends =
+      NodeFr(1,
+        List( NodeFr(2, List()), NodeFr(3, List( NodeFr(2, List()), NodeFr(4, List( NodeFr(5, List( NodeFr(6, List()))), NodeFr(2, List()))), NodeFr(5, List( NodeFr(6, List()))) ))))
+
+    println("test drzewa znajomych")
+    println(deleteDuplicateDepth(treeFriends))
+    println(deleteDuplicatesBreadth(treeFriends))
+
+    Node(1,Node(2,
+      Empty,
+      Node(3,
+        Empty,
+        Empty)),
+      Node(4,
+        Node(5,
+          Empty,
+          Empty),
+        Node(6,
+          Empty,
+          Empty)))
 
 
   }
