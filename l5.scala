8b97454a6c7485c44e091187816d36702c576ed8
NeverGiveUPek <pdruczkin@gmail.com>
2021-11-30 17:43:09 +0100

    Add ex 2,3,4

--- l5.scala
+++ l5.scala
@@ -6,6 +6,17 @@ object Main {
     println(convertTo16(31))
     println(convertNumber(31, 16))
 
+    val treeTest = Node(1.0, Node(2.0,Leaf,Leaf), Node(3.0,Leaf,Leaf))
+    val tree = createTree(4)
+
+    println(breadthPrint(treeTest))
+    println(inorderProduct(treeTest))
+
+    println(breadthPrint(tree))
+    println(inorderProduct(tree))
+
+
+
 
   }
 }
@@ -28,6 +39,59 @@ def convertNumber(number: Int, base: Int) =
     else resultList
 
   converNumberHelper(number,Nil)
+
+
+
+
+sealed trait BinaryTree[+A]
+case object Leaf extends BinaryTree[Nothing]
+case class Node[A](value: A, left: BinaryTree[A], right: BinaryTree[A]) extends BinaryTree[A]
+
+
+def getRandomNumber() =
+  Math.floor(Math.random() * 100) / 100
+
+
+
+def createTree(height :Int):BinaryTree[Double] =
+  def createTreeNext(currentHeight :Int):BinaryTree[Double] =
+    if(currentHeight == height) then Leaf
+    else Node(Math.random(), createTreeNext(currentHeight + 1), createTreeNext(currentHeight + 1))
+
+  createTreeNext(0)
+
+
+def inorderProduct(tree: BinaryTree[Double]) =
+  def inorderHelper(node: BinaryTree[Double]):Double =
+    node match
+      case Leaf => 1;
+      case Node(value, left, right) => value * inorderHelper(left) * inorderHelper(right)
+
+  inorderHelper(tree)
+
+
+//numbers are printed with 2 dig after comma
+def breadthPrint(tree: BinaryTree[Double]) =
+  def breadthHelper(listOfNodes: List[BinaryTree[Double]]):List[Double] =
+    listOfNodes match
+      case Nil => Nil
+      case Leaf :: tail => breadthHelper(tail)
+      case Node(value, left , right) :: tail => (Math.floor(value * 100) / 100) :: breadthHelper(tail ::: (List(left,right)))
+
+  breadthHelper(List(tree))
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
