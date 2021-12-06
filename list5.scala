5183dbcf34e1428385b19b44739e05eccd1bec9a
Monika <260278@student.pwr.edu.pl>
2021-12-06 07:50:05 +0100

    improve DeletingByDFS and decToAny function

--- list5.scala
+++ list5.scala
@@ -15,13 +15,16 @@ object list5 {
 
   def decToHexBetter(dec: Int): List[Int]={
     @tailrec
-    def decToHexBetterHel(dec2: Int, list: List[Int]): List[Int]={
-      if dec2<16 then list
-      else decToHexBetterHel(dec2/16, dec2%16::list)
+    def decToHexBetterHel(dec: Int, list: List[Int]): List[Int]={
+      if dec<16 then dec::list
+      else if dec<0 then decToHexBetterHel((-dec)/16, (-dec%16)::list:::List(-1))
+      else decToHexBetterHel(dec/16, dec%16::list)
     }
-    decToHexBetterHel(dec, List()).reverse
+    var result = decToHexBetterHel(dec, List())
+    if result.reverse.head == -1 then -1::result.reverse.tail.reverse
+    else result
   }
-  //zadanie 1 - mod
+  //zadanie 1 - mod *
   def decToAny(dec:Int, sys:Int):List[Int]={
     def decToAnyHelp(dec:Int):List[Int]={
       if dec==0 then Nil
@@ -32,11 +35,15 @@ object list5 {
 
   def decToAnyBetter(dec:Int, sys:Int):List[Int]={
     @tailrec
-    def decToAnyHelp(dec2:Int, list: List[Int]):List[Int]={
-      if dec2<sys then list
-      else decToAnyHelp(dec2/sys, dec2%sys::list)
+    def decToAnyHelp(dec:Int, list: List[Int]):List[Int]={
+      if sys<0 then throw new Exception("Negative base")
+      else if dec<0 then decToAnyHelp(-dec/sys, (-dec%(sys))::list:::List(-1))
+      else if dec<sys then dec::list
+      else decToAnyHelp(dec/sys, dec%sys::list)
     }
-    decToAnyHelp(dec, List()).reverse
+    var result = decToAnyHelp(dec, List())
+    if result.reverse.head == -1 then -1::result.reverse.tail.reverse
+    else result
   }
   //zadanie 3
   sealed trait BT[+A]
@@ -49,14 +56,14 @@ object list5 {
     else if height==0 then Empty
     else Node(value, Empty, Empty)
   }
-  def breadthBT[A](bt: BT[A]): List[A] ={
+  def BFS[A](bt: BT[A]): List[A] ={
     @tailrec
-    def bread[A](queue: List[BT[A]], result: List[A]):List[A]={
-      if queue==Nil then result
+    def bread[A](queue: List[BT[A]], result: List[A]):List[A]= {
+      if queue == Nil then result
       else
         queue.head match {
           case Empty => bread(queue.tail, result)
-          case Node(elem, left, right) => bread(queue.tail:::List(left, right), elem::result)
+          case Node(elem, left, right) => bread(queue.tail ::: List(left, right), elem :: result)
         }
     }
     bread(List(bt), List()).reverse
@@ -145,50 +152,84 @@ object list5 {
     Helper(bt, List())
   }
 
-  def DeletingByBfs(bt: BT[Int]): BT[Int]= {
-    def Helper(queue: List[Int], listElements: List[Int], visited: List[Int]): (List[Int])= {
+  def insert(tree: BT[Int], elem:Int): BT[Int]={
+    tree match{
+      case Empty => Node(elem, Empty, Empty)
+      case Node(root, right, left) => if root<elem then Node(root, right, insert(left, elem)) else
+        Node(root, insert(right, elem), left)
+    }
+  }
+  def buildTree(queue: List[Int], tree: BT[Int]): BT[Int]={
+    queue match{
+      case Nil => tree
+      case h::t => buildTree(t, insert(tree, h))
+    }
+  }
+
+  def DFS[A](bt: BT[A]): (List[A])={
+    @tailrec
+    def DFSHelper(stack: List[BT[A]], result: List[A]): (List[A]) ={
+      if stack==Nil then result
+      else
+        stack.head match {
+          case Empty => DFSHelper(stack.tail, result)
+          case Node(elem, left, right) => DFSHelper(List(left, right):::stack.tail, elem::result)
+        }
+    }
+    DFSHelper(List(bt), List()).reverse
+  }
+  /*def DeletingByDfs(bt :BT[Int]): (BT[Int],List[Int])= {
+    def DfsInner(stack: List[Int], listElements: List[Int], visited: List[Int]): (List[Int])= {
+      stack match{
+        case Nil => visited
+        case h::t => if(check(listElements, h)==true) then DfsInner(t, listElements, visited) else
+          DfsInner(t, h::listElements, visited:::List(h))
+      }
+    }
+    (buildTree(DfsInner(DFS(bt), List(), List()), Empty), DfsInner(DFS(bt), List(), List()))
+
+  }*/
+
+  def DeletingByBfs(bt: BT[Int]): (BT[Int])= {
+    def BfsInner(queue: List[Int], listElements: List[Int], visited: List[Int]): (List[Int])= {
       queue match {
         case Nil => visited
-        case h::t => if(check(listElements, h)==true) then Helper(t, listElements, visited) else Helper(t, h::listElements, visited:::List(h))
+        case h::t => if(check(listElements, h)==true) then BfsInner(t, listElements, visited) else BfsInner(t, h::listElements, visited:::List(h))
       }
     }
-    def insert(tree: BT[Int], elem:Int): BT[Int]={
-      tree match{
-        case Empty => Node(elem, Empty, Empty)
-        case Node(root, right, left) => if root<elem then Node(root, right, insert(left, elem)) else
-          Node(root, insert(right, elem), left)
-      }
-    }
-    def buildTree(queue: List[Int], tree: BT[Int]): BT[Int]={
-      queue match{
-        case Nil => tree
-        case h::t => buildTree(t, insert(tree, h))
-      }
-    }
-    buildTree(Helper(breadthBT(bt), List(), List()), Empty)
+    buildTree(BfsInner(BFS(bt), List(), List()), Empty)
   }
 
   def main(args: Array[String]): Unit = {
     println(decToHexBetter(31))
     println(decToHexBetter(100))
     println(decToAnyBetter(31, 16))
-    println(decToAnyBetter(100,4))
-    println(breadthBT(createTree(3, 0, 1)))
+    println(decToAnyBetter(-2936, 16))
+    println(decToAnyBetter(100,8))
+    println(BFS(createTree(3, 0, 1)))
     val t = createTree(3, 0, 1)
-    println(breadthBT(t))
+    println(BFS(t))
     println(multiplyNodes(t))
     println("")
 
-    var t2 = createTreeInt(3, 0, 20)
-    println(breadthBT(t2))
+    println("Deleting by DFS and BFS:")
+    var t2 = createTreeInt(3, 0, 15)
+    var tt2 = buildTree(BFS(t2), Empty)
+    println(BFS(tt2))
+    println(DFS(t2))
     println(t2)
-    println(breadthBT(DeletingByDfs(t2)))
-    println(DeletingByDfs(t2))
+    println("Deleted by BFS:")
+    println((DeletingByBfs(t2)))
+    //println(BFS(DeletingByBfs(t2)))
+    println("Deleted by DFS:")
+    println((DeletingByDfs(tt2)))
+    //println(BFS(DeletingByDfs(tt2)))
+    /*println("Deleting by BFS:")
     var t3 = createTreeInt(3, 0, 20)
     println(t3)
-    println(breadthBT(t3))
+    println(BFS(t3))
     println(DeletingByBfs(t3))
-    println(breadthBT(DeletingByBfs(t3)))
+    println(BFS(DeletingByBfs(t3)))*/
   }
 
 }
