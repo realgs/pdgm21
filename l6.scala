c9a2439f72573e5b0ba4564fd1ff25dc84aff9fe
NeverGiveUPek <pdruczkin@gmail.com>
2021-12-14 10:35:27 +0100

    Add tests and little repair

--- l6.scala
+++ l6.scala
@@ -1,37 +1,99 @@
 import java.util
+import scala.List
 
 object Main {
   def main(args: Array[String]): Unit = {
-    val list = LazyList(5,4,3,2,1)
-    val result = eachNElement(list,5,2).toList
+    println("--------- 1")
+    val list = LazyList(5,6,3,2,1)
+    val result = eachNElement(list,3,2).toList
     println(result)
 
-    val first = LazyList(5,2,0)
+    println()
+
+    val result2 = eachNElement(list,4,2).toList
+    println(result2)
+
+    println()
+
+    val result3 = eachNElement(list, -1, 2).toList
+    println(result3)
+
+    println()
+
+    val result4 = eachNElement(list, 5, -1).toList
+    println(result4)
+
+
+    println("------------------------")
+
+
+
+
+    println("--------- 2")
+    val first = LazyList(1,2,3)
     val second = LazyList(2,3,4,5)
-    val addResult = lazyExecute(first,second,'/')
+    val addResult = lazyExecute(first,second,'+')
     println(addResult.toList)
 
+    println()
+
+    val first1 = LazyList()
+    val second1 = LazyList(1,2,3,4,5)
+    val divResult = lazyExecute(first1, second1, '/')
+    println(divResult.toList)
+
+    println()
+
+    val first2 = LazyList(2,2,2,2)
+    val second2 = LazyList(1,0,-1)
+    val mulResult = lazyExecute(first2, second2, '*')
+    println(mulResult.toList)
+
+    println("------------------------")
+    println("--------- 3")
 
     val listToDuplicate = List(1,2,3)
-    val duplicators = List(2,2,2)
+    val duplicators = List(0,3,1,4)
 
     val duplicatedResult = duplicate(listToDuplicate,duplicators)
     println(duplicatedResult)
+
+    println()
+
+    val listToDuplicate1 = List("A","B","C")
+    val duplicators1 = List(1,2)
+
+    val duplicatedResult1 = duplicate(listToDuplicate1,duplicators1)
+    println(duplicatedResult1)
+
+    println("------------------------")
+    println("--------- 4/5")
+
+
+    var p = new Point(3,4)
+    println(p.debugName())
+    println()
+    println(p.debugVars())
+
+    println("------------------------")
+
   }
 }
 
 
-def eachNElement[A](list :LazyList[A], m :Int, n :Int) =
-  def eachNElementHelper[A](list :LazyList[A], currentM :Int, currentN :Int): LazyList[A] =
-    (list,currentM) match
+def eachNElement[A](list :LazyList[A], end :Int, gap :Int) =
+  def eachNElementHelper[A](list :LazyList[A], endElem :Int, currentGap :Int): LazyList[A] =
+    (list,endElem) match
       case (LazyList(),_) => LazyList()
       case (h #:: t, 0) => LazyList()
       case (h #:: t,_) =>
-        if currentN == 1 then h #:: eachNElementHelper(t, currentM - 1, n)
-        else  eachNElementHelper(t, currentM - 1, currentN - 1)
+        if currentGap == 1 then h #:: eachNElementHelper(t, endElem - 1, gap)
+        else  eachNElementHelper(t, endElem - 1, currentGap - 1)
 
 
-  eachNElementHelper(list, m, 1)
+  if end >= 0 && gap > 0 then eachNElementHelper(list, end, 1)
+  else LazyList()
+
 
 
 
@@ -54,6 +116,35 @@ def duplicate[A](list: List[A], copyNumbers :List[Int]): List[A] =
   (list zip copyNumbers).flatMap((a,n) => copy(a,n))
 
 
+
+
+
+trait Debug {
+  def debugName() = this.getClass().getName().toString
+
+  def debugVars1() =
+    val fields = this.getClass().getDeclaredFields.toList
+    val fieldsNames = fields.map(f => f.getName().toString)
+    val fieldsClass = fields.map(f => f.getAnnotatedType().toString)
+    fields.foreach(f => f.setAccessible(true))
+    val fieldsValues = fields.map(f => f.get(this))
+
+    fieldsNames.lazyZip(fieldsClass).lazyZip(fieldsValues).toList
+
+  def debugVars() = this.getClass().getDeclaredFields.toList
+    .map(f =>
+      f.setAccessible(true)
+      List(f.getName(), f.getAnnotatedType().toString, f.get(this))
+    )
+
+}
+
+
+class Point(xv: Int, yv: Int) extends Debug {
+  var x: Int = xv
+  var y: Int = yv
+  var a: String = "test"
+}
 
 
 
