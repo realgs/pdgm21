81a73f65f5245225640b48b21de0cc0a888c9f0a
Adwin120 <adam.winnik120@gmail.com>
2021-12-16 09:57:58 +0100

    finish list

--- lista6.scala
+++ lista6.scala
@@ -3,6 +3,7 @@ import scala.annotation.tailrec
 object lista6 {
 
   def eachNElement[A](list: LazyList[A], step: Int, end: Int) =
+    if step <= 0 then throw new IllegalArgumentException("step must be positive")
     def eachNRec(list: LazyList[A], i: Int, endCounter: Int): LazyList[A] =
       if endCounter <= 0 || list.isEmpty then LazyList()
       else if i == 0 then list.head #:: eachNRec(list.tail, step - 1, endCounter - 1)
@@ -27,16 +28,49 @@ object lista6 {
     lazyZipWithCallback(arithmeticOperatorCharToFunction(operator))(firstList, secondList)
 
   def duplicate[A](elements: LazyList[A], counts: LazyList[Int]): LazyList[A] =
-    def duplicateRec(elements: LazyList[A], counts: LazyList[Int], currCount: Int): LazyList[A] =
-      if elements.isEmpty || counts.isEmpty then LazyList()
-      else if currCount <= 0 then duplicateRec(elements.tail, counts.tail, counts.tail.head)
-      else elements.head #:: duplicateRec(elements, counts, currCount - 1)
+    (elements, counts) match
+      case (elemHd #:: elemTl, countHd #:: countTl) =>
+        if countHd <= 0 then duplicate(elemTl, countTl)
+        else elemHd #:: duplicate(elements, (countHd - 1) #:: countTl)
+      case _ => LazyList()
 
-    duplicateRec(elements, counts, counts.head)
+  trait Debug:
+    def debugName(): String =
+      getClass().getName()
+
+    def debugVars() =
+      for(field <- getClass().getDeclaredFields()) yield {
+        field.setAccessible(true)
+        Array(field.getName(), field.getGenericType(), field.get(this))
+      }
+
+  class Point(xv: Int, yv: Int) extends Debug {
+    var x: Int = xv
+    var y: Int = yv
+    var a: String = "test"
+  }
 
   def main(args: Array[String]): Unit =
     println(eachNElement(LazyList(5,6,3,2,1), 2, 3).force)
     println(eachNElement(LazyList(5,6,3,2,1), 2, 4).force)
+    println(eachNElement(LazyList(5,6,3,2,1), 2, 5).force)
+    println(eachNElement(LazyList(5,6,3,2,1), 1, 3).force)
+    println(eachNElement(LazyList(5,6,3,2,1,0,-1,-2), 3, 7).force)
+
+    println("-------------")
+
     println(lazyExecute(LazyList(1,2,3), LazyList(2,3,4,5), '+').force)
+    println(lazyExecute(LazyList(1,2,3), LazyList(2,3,4,5), '/').force)
+    println(lazyExecute(LazyList(1,2,3,4,5,6,7), LazyList(2,3,4), '-').force)
+
+    println("-------------")
+
     println(duplicate(LazyList(1,2,3), LazyList(0,3,1,4)).force)
+    println(duplicate(LazyList(1,2,3,4,5,6), LazyList(0,3,1,4)).force)
+
+    println("-------------")
+
+    var p : Point = new Point(3, 4)
+    println(p.debugName())
+    for(devvar <- p.debugVars()) yield println(devvar.mkString(" "))
 }
