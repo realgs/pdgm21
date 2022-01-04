0a09c4e165573984f65b1f9024ffa17a9ade41a0
234mati <mateusz.pietrych@gmail.com>
2022-01-04 01:02:07 +0100

    Amendments

--- src/ListLab7.scala
+++ src/ListLab7.scala
@@ -6,7 +6,6 @@ import scala.concurrent.{Await, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
 
 
-
 object ListLab7 {
 
   sealed trait BT[+A]
@@ -49,6 +48,35 @@ object ListLab7 {
       Await.result(f2, Duration.Inf).toString
     }
     println("\n")
+    //
+    //
+    //    println("PRZERWA")
+    //    time{
+    //      val f2 = Future{MaxInTreeTime2(tree,1) }
+    //      println(Await.result(f2, Duration.Inf))
+    //      Await.result(f2, Duration.Inf).toString
+    //    }
+    //    time{
+    //      val f2 = Future{MaxInTreeTime2(tree,2) }
+    //      println(Await.result(f2, Duration.Inf))
+    //      Await.result(f2, Duration.Inf).toString
+    //    }
+    //    time{
+    //      val f2 = Future{MaxInTreeTime2(tree,3) }
+    //      println(Await.result(f2, Duration.Inf))
+    //      Await.result(f2, Duration.Inf).toString
+    //    }
+    //    time{
+    //      val f2 = Future{MaxInTreeTime2(tree,4) }
+    //      println(Await.result(f2, Duration.Inf))
+    //      Await.result(f2, Duration.Inf).toString
+    //    }
+    //    time{
+    //      val f2 = Future{MaxInTreeTime2(tree,5) }
+    //      println(Await.result(f2, Duration.Inf))
+    //      Await.result(f2, Duration.Inf).toString
+    //    }
+    //    println("\n")
     var list = List(3,6,2,6,4,8,4,3,5,9,1)
     var list2 = List(3,6,2,6,4,8,4,3,5,9,1)
     time{
@@ -64,6 +92,18 @@ object ListLab7 {
     }
     time{
       val f = Future{mergeSortTime(createList(3000),3)}
+      println(Await.result(f, Duration.Inf))
+      Await.result(f, Duration.Inf).toString
+    }
+    println("\n")
+    println("PRZERWA")
+    time{
+      val f = Future{mergeSortTime2(createList(3000),2)}
+      println(Await.result(f, Duration.Inf))
+      Await.result(f, Duration.Inf).toString
+    }
+    time{
+      val f = Future{mergeSortTime2(createList(3000),3)}
       println(Await.result(f, Duration.Inf))
       Await.result(f, Duration.Inf).toString
     }
@@ -93,24 +133,40 @@ object ListLab7 {
   def MaxInTree(tree: BT[Double]): Double =
     tree match
       case Empty => 0
-      case Node(v,left,right) => val x = MaxInTree(left)
-        val y = MaxInTree(right)
-        if v > x && v > y then v
-        else if x > y then x
-        else y
+      case Node(value,left,right) => val maxl = MaxInTree(left)
+        val maxr = MaxInTree(right)
+        if value > maxl && value > maxr then value
+        else if maxl > maxr then maxl
+        else maxr
 
-  def MaxInTreeTime(tree: BT[Double],x: Int): Double =
-    if x <= 0 then
+  //  def MaxInTreeTime(tree: BT[Double],x: Int): Double =
+  //    if x <= 0 then
+  //      val f = Future{ MaxInTree(tree) }
+  //      Await.result(f,Duration.Inf)
+  //    else
+  //      tree match
+  //        case Empty => 0
+  //        case Node(value,left,right) => val maxl = MaxInTreeTime(left,x-1)
+  //          val maxr = MaxInTreeTime(right,x-1)
+  //          if value > maxl && value > maxr then value
+  //          else if maxl > maxr then maxl
+  //          else maxr
+
+  def MaxInTreeTime(tree: BT[Double], deep: Int): Double =
+    if deep <= 0 then
       val f = Future{ MaxInTree(tree) }
       Await.result(f,Duration.Inf)
     else
       tree match
         case Empty => 0
-        case Node(v,left,right) => val k = MaxInTreeTime(left,x-1)
-          val y = MaxInTreeTime(right,x-1)
-          if v > k && v > y then v
-          else if k > y then k
-          else y
+        case Node(value,left,right) => val f1 = Future{MaxInTreeTime(left,deep-1)}
+          val f2 = Future{MaxInTreeTime(right,deep-1)}
+          val maxl = Await.result(f1,Duration.Inf)
+          val maxr = Await.result(f2,Duration.Inf)
+          if value > maxl && value > maxr then value
+          else if maxl > maxr then maxl
+          else maxr
+
 
   def splitList[A](list: List[A]): (List[A],List[A],Int) =
     val length = list.length;
@@ -123,23 +179,35 @@ object ListLab7 {
       case (Nil,l) => l
       case (l,Nil) => l
       case (Nil,Nil) => Nil
-      case (h1::t1, h2::t2) => if h1<h2 then h1::combineLists(lista.tail,listb)
-                               else if h1==h2 then h1::h2::combineLists(lista.tail,listb.tail)
-                               else h2::combineLists(lista,listb.tail)
+      case (h1::t1, h2::t2) =>
+        if h1<h2 then h1::combineLists(lista.tail,listb)
+        else if h1==h2 then h1::h2::combineLists(lista.tail,listb.tail)
+        else h2::combineLists(lista,listb.tail)
+
 
 
   def mergeSort(list: List[Int]): List[Int] =
-    val x = splitList(list)
-    if x._3 > 0 then combineLists(mergeSort(x._1) ,mergeSort(x._2)) else List(list.head)
+    val splittedLists = splitList(list)
+    if splittedLists._3 > 0 then combineLists(mergeSort(splittedLists._1) ,mergeSort(splittedLists._2)) else List(list.head)
 
 
-  def mergeSortTime(list: List[Int], n: Int): List[Int] =
-    if n > 0 then
-               val x = splitList(list)
-               if x._3 > 0 then combineLists(mergeSortTime(x._1,n-1) ,mergeSortTime(x._2,n-1)) else List(list.head)
+  def mergeSortTime(list: List[Int], deep: Int): List[Int] =
+    if deep > 0 then
+      val splittedLists = splitList(list)
+      if splittedLists._3 > 0 then combineLists(mergeSortTime(splittedLists._1,deep-1) ,mergeSortTime(splittedLists._2,deep-1)) else List(list.head)
     else
       val f = Future{ mergeSort(list) }
       Await.result(f,Duration.Inf)
 
 
+  def mergeSortTime2(list: List[Int], deep: Int): List[Int] =
+    if deep > 0 then
+      val x = splitList(list)
+      if x._3 > 0 then
+        val f1 = Future{mergeSortTime2(x._1,deep-1) }
+        val f2 = Future{mergeSortTime2(x._2,deep-1) }
+        combineLists(Await.result(f1,Duration.Inf),Await.result(f2,Duration.Inf)) else List(list.head)
+    else
+      val f = Future{ mergeSort(list) }
+      Await.result(f,Duration.Inf)
 }
