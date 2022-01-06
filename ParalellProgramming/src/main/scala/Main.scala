135d3731e29a4d74994c1aa199f02b93b8689b29
Filip1159 <fwfw541111@gmail.com>
2022-01-07 00:13:20 +0100

    Add Quicksorter

--- ParalellProgramming/src/main/scala/Main.scala
+++ ParalellProgramming/src/main/scala/Main.scala
@@ -1,3 +1,46 @@
-object Main {
+import scala.util.Random
 
-}
+object Main:
+  def main(args: Array[String]): Unit =
+    val matrix1 = Array.ofDim[Int](1500, 500)
+    val matrix2 = Array.ofDim[Int](500, 2000)
+
+    for (i <- matrix1.indices)
+      for (j <- matrix1(0).indices)
+        matrix1(i)(j) = i + j
+
+    for (i <- matrix2.indices)
+      for (j <- matrix2(0).indices)
+        matrix2(i)(j) = i + j
+
+    val multiplicator = new MatrixMultiplicator(matrix1, matrix2)
+
+    println(" ==== Measuring matrix multiplicator ==== ")
+    var start = System.currentTimeMillis()
+    multiplicator.multiply()
+    println(s"Single thread result: ${System.currentTimeMillis() - start}ms")
+
+    start = System.currentTimeMillis()
+    multiplicator.multiplyParallel()
+    println(s"Dual thread result: ${System.currentTimeMillis() - start}ms")
+
+    val array = Array.ofDim[Int](10*1000*1000)
+    val r = new Random()
+    for (i <- array.indices)
+      array(i) = r.nextInt()
+
+    val quicksorter = new Quicksorter()
+
+    println(" ==== Measuring quicksorter ==== ")
+    start = System.currentTimeMillis()
+    quicksorter.quicksort(array)
+    println(s"Single thread result: ${System.currentTimeMillis() - start}ms")
+
+    for (i <- array.indices)
+      array(i) = r.nextInt()
+
+    start = System.currentTimeMillis()
+    quicksorter.quicksortParalell(array)
+    println(s"Dual thread result: ${System.currentTimeMillis() - start}ms")
+
+
---
+++ ParalellProgramming/src/main/scala/Quicksorter.scala
@@ -0,0 +1,41 @@
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration.Duration
+import concurrent.ExecutionContext.Implicits.global
+import scala.util.Random
+
+class Quicksorter:
+  val r = new Random
+
+  private def partition(arr: Array[Int], begin: Int, end: Int): Int =
+    val pivot = arr(begin + r.nextInt(end-begin))
+    var i = begin - 1
+    for (j <- begin until end)
+      if arr(j) <= pivot then
+        i += 1
+        val swapTemp = arr(i)
+        arr(i) = arr(j)
+        arr(j) = swapTemp
+    val swapTemp = arr(i + 1)
+    arr(i + 1) = arr(end)
+    arr(end) = swapTemp
+    i + 1
+
+  private def sort(arr: Array[Int], begin: Int, end: Int): Unit =
+    if begin < end then
+      val partitionIndex = partition(arr, begin, end)
+      sort(arr, begin, partitionIndex - 1)
+      sort(arr, partitionIndex + 1, end)
+
+  def quicksort(arr: Array[Int]): Unit =
+    sort(arr, 0, arr.length-1)
+
+  def quicksortParalell(arr: Array[Int]): Unit =
+    val partitionIndex = partition(arr, 0, arr.length-1)
+    val f1 = Future {
+      sort(arr, 0, partitionIndex - 1)
+    }
+    val f2 = Future {
+      sort(arr, partitionIndex + 1, arr.length-1)
+    }
+    Await.result(f1, Duration.Inf)
+    Await.result(f2, Duration.Inf)
