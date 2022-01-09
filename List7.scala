40c4bf66e86eaadd0ae1adf070edb46719784dd3
Szymek <shibek123@gmail.com>
2022-01-09 14:25:11 +0100

    Add MaxElem

--- List7.scala
+++ List7.scala
@@ -105,15 +105,37 @@ object List7 {
     val split = array.length/2
     val f1 = Future(count(0, split, array))
     val f2 = Future(count(split+1, array.length - 1, array))
-    Await.result(f1,1000 seconds)+Await.result(f2, 1000 seconds)
+    Await.result(f1,Duration.Inf)+Await.result(f2, Duration.Inf)
+
+  def maxElem(array: Array[Int]): Int =
+    var max = array(0)
+    for(elem <- array) if elem > max then max = elem
+    max
+
+  def maxElemParallel(array: Array[Int]): Int =
+    def search(start: Int, end: Int, array: Array[Int]): Int =
+      var max = array(start)
+      var i = start
+      while (i <= end)
+        if array(i) > max then max = array(i)
+        i +=1
+      max
+    val split = array.length/2
+    val res1 = Await.result(Future(search(0, split, array)), Duration.Inf)
+    val res2 = Await.result(Future(search(split+1, array.length - 1, array)), Duration.Inf)
+    if res1 > res2 then res1 else res2
+
+  def slowFunction(i: Int) = { Thread.sleep(10000); "hello" + i + "world" }
+
 
   def main(args: Array[String]): Unit = {
 
     var start = System.currentTimeMillis()
     var end = System.currentTimeMillis()
 
-    val array = Array.range(1, 501)
+    var array = Array.range(1, 100001)
 
+    println("SumArray: 100000 elems")
     start = System.currentTimeMillis()
     println(sumArray(array))
     end = System.currentTimeMillis()
@@ -124,38 +146,25 @@ object List7 {
     end = System.currentTimeMillis()
     println("TimeParallel: " + (end-start))
 
-/*
-    var array0 = Array.fill(10)(Random.nextInt(10))
+    array = Array.range(1, 10000001)
+
+    println("SumArray 10000000 elems")
+    start = System.currentTimeMillis()
+    println(sumArray(array))
+    end = System.currentTimeMillis()
+    println("Time: " + (end-start))
+
+    start = System.currentTimeMillis()
+    println(sumArrayParallel(array))
+    end = System.currentTimeMillis()
+    println("TimeParallel: " + (end-start))
+
+    var array0 = Array.fill(1000)(Random.nextInt(10000))
     var array1 = array0.clone()
     var array2 = array0.clone()
     var array3 = array0.clone()
 
-    println("10 elems")
-    start = System.currentTimeMillis()
-    quicksort(array0)
-    end = System.currentTimeMillis()
-    println("Time1: " + (end-start))
-
-    start = System.currentTimeMillis()
-    quicksortParallel(array1)
-    end = System.currentTimeMillis()
-    println("Time2: " + (end-start))
-
-    start = System.currentTimeMillis()
-    quicksortParallel2(array2)
-    end = System.currentTimeMillis()
-    println("Time3 " + (end-start))
-
-    start = System.currentTimeMillis()
-    quicksortParallel3(array3)
-    end = System.currentTimeMillis()
-    println("Time4 " + (end-start))
-
-    array0 = Array.fill(1000)(Random.nextInt(1000))
-    array1 = array0.clone()
-    array2 = array0.clone()
-    array3 = array0.clone()
-
+    print("Quicksort:")
     println("1000 elems")
 
     start = System.currentTimeMillis()
@@ -231,7 +240,17 @@ object List7 {
     quicksortParallel3(array3)
     end = System.currentTimeMillis()
     println("Time4 " + (end-start))
-*/
+
+    println("MaxElem:")
+    start = System.currentTimeMillis()
+    println(maxElem(array0))
+    end = System.currentTimeMillis()
+    println("Time: " + (end-start))
+
+    start = System.currentTimeMillis()
+    println(maxElemParallel(array0))
+    end = System.currentTimeMillis()
+    println("TimeParallel: " + (end-start))
   }
 
   }
