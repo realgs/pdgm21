2a2bda9473dca8823741bf402ec78427a8e86b3a
Wiktor Sadowy <wiktor.sadowy@onet.pl>
2022-01-09 20:03:02 +0100

    Kalaha without GUI finish

--- src/main/scala/StartGame.scala
+++ src/main/scala/StartGame.scala
@@ -6,7 +6,7 @@ import player.AIPlayer
 object StartGame {
   def main(args: Array[String]): Unit = {
     val server = Server()
-    server.initializeGame(6, 5, false, false)
+    server.initializeGame(6, 5, true, true)
     server.playGame()
   }
 }
--- src/main/scala/player/HumanPlayer.scala
+++ src/main/scala/player/HumanPlayer.scala
@@ -2,7 +2,21 @@ package player
 import gameboard.KalahaBoard
 
 class HumanPlayer (firstPlayer: Boolean) extends Player (firstPlayer) {
-  // Human players start indexing at 1 not 0
   override def chooseMove(kalahaBoard: KalahaBoard): Int =
-    return 1
+    var numberIsInteger: Boolean = false
+    var chosenHole: Int = -1
+    while (!numberIsInteger) {
+      println("Choose hole number (hole to the left is hole number 1, hole next to it is 2 and so on)")
+      val input: String = scala.io.StdIn.readLine()
+      try {
+        chosenHole = input.toInt
+        numberIsInteger = true
+      }
+      catch {
+        case _: NumberFormatException =>
+      }
+    }
+
+    // Human player start indexing from 1 not 0
+    return chosenHole-1
 }
