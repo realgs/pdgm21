aa0b508b65b591b798978d0ff6a2d5c88db05477
DogeXD <44587062+DogeXD@users.noreply.github.com>
2022-01-14 21:19:40 +0100

    deleted old stuff

--- Main.scala
+++ Main.scala
@@ -8,53 +8,13 @@ object Main {
 
 
 
-  def runAuto(): Unit ={
-    val server = new Server
-    val player1 = new PlayerHuman(server,1)
-    //val player1= new PlayerAI(server,1)
-    val player2 = new PlayerAI(server,2)
-    //server.aiVsAi(player1,player2)
-    server.play(player1,player2)
-  }
 
 
-  def debugScenario(): Unit ={
-    val a = new Game()
-    a.start()
-    val gamestate =  a.move(0,1)
-    a.printGameStatus()
-    println(gamestate.gameBoard.toList)
-  }
-  def treeTes(): Unit ={
-    //val tree = DecisionTree(0,)
-  }
   def main(args :Array[String]): Unit = {
+    val server  = new Server()
+    val player1 = new PlayerHuman(server,1)
+    val player2 =new  PlayerAI(server,2)
+    server.play(player1,player2)
 
-    //test tree nodes
-
-    //end
-
-      runAuto()
-      //debugScenario()
-//    val a = new Game()
-//    a.start()
-//    println("podaj pole: ")
-//    var field = scala.io.StdIn.readInt()
-//    println("podaj gracza: ")
-//    var player = scala.io.StdIn.readInt()
-//    a.move(field, player)
-//    a.printGameStatus()
-//
-//
-//    while (player != -1) {
-//      println("podaj pole: ")
-//      field = scala.io.StdIn.readInt()
-//      println("podaj gracza: ")
-//      player = scala.io.StdIn.readInt()
-//      a.move(field, player)
-//      a.printGameStatus()
-//
-//
-//    }
   }
 }
