a9e4b9758557397a546945549c6083cd3deb87ce
Pitru <260371@student.pwr.edu.pl>
2022-01-15 20:44:34 +0100

    Add test game to game class

--- src/Game.scala
+++ src/Game.scala
@@ -3,20 +3,20 @@ import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 
 class Game:
-	var board = new Board(6,this)
+	var board = new Board(6, this)
 
-	def gameEnd(result: Int): Unit=
+	def gameEnd(result: Int): Unit =
 		result match
-			case  0 => println("REMIS")
-			case  1 => println("PLAYER A WON")
-			case  2 => println("PLAYER B WON")
+			case 0 => println("REMIS")
+			case 1 => println("PLAYER A WON")
+			case 2 => println("PLAYER B WON")
 
-	def printBoard(): Unit=
-		println(board.boardString())
+	def printBoard(): Unit =
+		println(board.boardToString())
 
-	def play()=
+	def play() =
 		var activePlayer = 0
-		while !board.anySiteClear() do
+		while !board.anySideClear() do
 			move(activePlayer)
 			activePlayer = enemyPlayer(activePlayer)
 			board.checkGameEnd(activePlayer)
@@ -33,7 +33,7 @@ class Game:
 			chooseHole()
 		else hole
 
-	def move(player: Int)=
+	def move(player: Int) =
 		printBoard()
 		println(s"PLAYER: ${player} MOVE")
 		val hole = chooseHole()
@@ -42,23 +42,23 @@ class Game:
 			chooseHole()*/
 		board.move(player, hole)
 
-	def start(): Unit=
+	def start(): Unit =
 		println("HELLO")
 		println("IT'S KALAHA GAME\n")
 		//setup()
 		play()
 
-	def setup(): Unit=
+	def setup(): Unit =
 		val players = setPlayersNumber()
-		val computers = 2-players
+		val computers = 2 - players
 		if players == 2 then
 			println("Two players")
 		else if players == 1 then
 			println("")
 		else
 			println("")
-		//2 computery
-		//let's play
+	//2 computery
+	//let's play
 
 	def setPlayersNumber(): Int =
 		val scanner = new Scanner(System.in)
@@ -69,5 +69,58 @@ class Game:
 			setPlayersNumber()
 		else alivePlayers
 
-    def test()=
+	def test(): Unit =
+		board.move(0, 4)
+		board.move(1, 1)
+		board.move(0, 1)
+		board.move(1, 0)
+		board.move(0, 2)
+		board.move(1, 5)
+		board.move(0, 3)
+		board.move(1, 3)
+		board.move(0, 0)
+		board.move(1, 3)
 
+		board.move(0, 5)
+		board.move(1, 2)
+		board.move(0, 1)
+		board.move(1, 3)
+		board.move(0, 2)
+		board.move(1, 0)
+		board.move(0, 4)
+		board.move(1, 1)
+		board.move(0, 5)
+		board.move(1, 4)
+
+		board.move(0, 1)
+		board.move(1, 2)
+		board.move(1, 4)
+		board.move(0, 0)
+		board.move(1, 1)
+		board.move(0, 4)
+		board.move(0, 2)
+		board.move(1, 3)
+		board.move(0, 5)
+		board.move(1, 0)
+
+		board.move(0, 0)
+		board.move(1, 2)
+		board.move(0, 3)
+		board.move(1, 4)
+		board.move(0, 4)
+		board.move(0, 1)
+		board.move(1, 1)
+		board.move(0, 5)
+		board.move(0, 2)
+		board.move(1, 3)
+
+		board.move(0, 3)
+		board.move(1, 5)
+		board.move(0, 4)
+		board.move(0, 3)
+		board.move(1, 0)
+		board.move(0, 2)
+		board.move(1, 1)
+		board.move(0, 1)
+		board.move(1, 2)
+		board.move(0, 5)
