5467a605afcc9b2f256655d5cf831a21623c2b19
Pitru <260371@student.pwr.edu.pl>
2022-01-15 19:24:56 +0100

    Working board with all move cases

--- src/Board.scala
+++ src/Board.scala
@@ -25,19 +25,28 @@ class Board(stones: Int, game: Game):
 		var stonesInHand = if player == 0 then playerAHoles(holeNumber) else playerBHoles(holeNumber)
 		val pos = playerAndPosToBoardPos(player, holeNumber)
 		setStones(pos, 0)
-		addStones(stonesInHand, pos+1)
+		addStones(player, stonesInHand, (pos + 1)%14, 0)
 
-		def addStones(leftStone: Int, boardPos: Int): Unit =
-			leftStone match
-				case 1 => handleEndMove(player, boardPos)
-				case _ => addStone(boardPos); addStones(leftStone - 1, (boardPos + 1) % 14)
 
-	def handleEndMove(player: Int, boardPos: Int): Unit =
+	def addStones(player: Int, leftStone: Int, boardPos: Int, przejazd: Int): Unit =
+		leftStone match
+			case 1 => handleEndMove(player, boardPos, przejazd)
+			case _ =>
+				if (player==0 && boardPos==13) || (player ==1 && boardPos == 6) then
+					addStones(player, leftStone, (boardPos + 1) % 14, przejazd)
+				else if (player==0 && boardPos==6) ||( player==1 &&boardPos==13) then
+					addStone(boardPos); addStones(player, leftStone - 1, (boardPos + 1) % 14, przejazd+1)
+				else
+					addStone(boardPos); addStones(player, leftStone - 1, (boardPos + 1) % 14, przejazd)
+
+	def handleEndMove(player: Int, boardPos: Int, przejazd: Int): Unit =
 		val endPos = posToPlayersHole(boardPos)
-		if endPos._2 == 6 && endPos._1 == player then
+		if endPos._2 == 6 && endPos._1 == player && przejazd==0 then
 			addStone(boardPos)
-			game.nextMove(player)
-		else if endPos._1 == player && getStonesNumberAtPos(boardPos) == 0 then
+			game.move(player)
+		else if endPos._2 ==6 && endPos._1 == enemyPlayer(player) then
+			addStones(player, 1, (boardPos+1)%14, przejazd)
+		else if endPos._1 == player && getStonesNumberAtPos(boardPos) == 0  && getStonesNumberAtPos(reversePos(boardPos)) != 0 then
 			val enemyPlayerHole = 5 - endPos._2
 			if player == 0 then
 				playerABase += 1
@@ -51,9 +60,22 @@ class Board(stones: Int, game: Game):
 				setStones(position, 0)
 		else addStone(boardPos)
 
+	def checkGameEnd(player: Int): Unit =
+		if playerSiteClear(player) then
+			if pointsDiff() == 0 then
+				game.gameEnd(0)
+			else if playerAPoints > playerBPoints then
+				game.gameEnd(1)
+			else
+				game.gameEnd(2)
+
+	def playerSiteClear(player: Int): Boolean =
+		if player == 0 then
+			playerAHoles.foldLeft(0)((p, n) => p + n) == 0
+		else playerBHoles.foldLeft(0)((p, n) => p + n) == 0
 
 	def playerAndPosToBoardPos(player: Int, hole: Int): Int =
-		player * 6 + hole
+		player * 7 + hole
 
 	def posToPlayersHole(pos: Int): (Int, Int) =
 		(pos / 7, pos % 7)
@@ -85,13 +107,20 @@ class Board(stones: Int, game: Game):
 	def enemyPlayer(player: Int): Int =
 		if player == 0 then 1 else 0
 
-	def boardString(): String=
-		val rowB = playerBHoles.foldLeft("")((row, number)=>String.format("| %2d ", number)+row)
-		val rowA = playerAHoles.foldLeft("")((row, number)=>row+String.format("| %2d ", number))
-		val b = String.format("| %2d |",playerBBase)
-		val a = String.format("| %2d |",playerABase)
-		"_________________________________________\n"+
-		s"|XXXX${rowB}|:pB |\n"+
-		s"${b}----|----|----|----|----|----${a}\n"+
-		s"| pA:${rowA}|XXXX|\n"+
-		"-----------------------------------------\n"
+	def reversePos(boardPos: Int): Int =
+		12-boardPos
+
+	def anySiteClear(): Boolean =
+		playerSiteClear(0) || playerSiteClear(1)
+
+	def boardString(): String =
+		val rowB = playerBHoles.foldLeft("")((row, number) => String.format("| %2d ", number) + row)
+		val rowA = playerAHoles.foldLeft("")((row, number) => row + String.format("| %2d ", number))
+		val b = String.format("| %2d |", playerBBase)
+		val a = String.format("| %2d |", playerABase)
+		"KALAHA GAME BOARD:\n"+
+		"_________________________________________\n" +
+			s"|XXXX${rowB}|:pB |\n" +
+			s"${b}----|----|----|----|----|----${a}\n" +
+			s"| pA:${rowA}|XXXX|\n" +
+			"-----------------------------------------\n\n"
