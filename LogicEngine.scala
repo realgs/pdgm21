515259e8283e6f762666c17d2123c009decfe377
Szymek <shibek123@gmail.com>
2022-01-15 12:34:34 +0100

    Repair AI bug

--- LogicEngine.scala
+++ LogicEngine.scala
@@ -1,21 +1,64 @@
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
+
 class LogicEngine(val game: Kalaha, val playerId: Int) {
 
-  var moves = List[Int]()
-  val decisions =new Tree(6)
-  val simulations = new Array[Kalaha](6)
+  var moves: List[Int] = List[Int]()
+  val decisions = new MyTree(6, game)
 
   def makeMove(): Int =
 
+    if !decisions.getNode(moves).hasKids then {
+    decisions.getNode(moves).setValue(game.getScore(playerId), game)
+
+    simulate()
+    }
+    var bestMove = -1
+    var bestScore = -100
+    val simResult = decisions.getNode(moves).findPaths()
+    simResult.foreach(elem =>
+      if elem._1._1 > bestScore && game.getField(playerId)(elem._2.head) != 0 then {
+        bestScore = elem._1._1
+        bestMove = elem._2.head
+      })
+    moves = moves :+ bestMove
+    bestMove
 
 
+  def simulate(): Unit =
+    def simulateMove(path: List[Int]): Unit =
+      /*
+      val move0 = simulateIteration(0, moves:::path)
+      val move1 = simulateIteration(1, moves:::path)
+      val move2 = simulateIteration(2, moves:::path)
+      val move3 = simulateIteration(3, moves:::path)
+      val move4 = simulateIteration(4, moves:::path)
+      val move5 = simulateIteration(5, moves:::path)
+      */
 
-    0
+      val move0 = Await.result(Future(simulateIteration(0, moves:::path)), Duration.Inf)
+      val move1 = Await.result(Future(simulateIteration(1, moves:::path)), Duration.Inf)
+      val move2 = Await.result(Future(simulateIteration(2, moves:::path)), Duration.Inf)
+      val move3 = Await.result(Future(simulateIteration(3, moves:::path)), Duration.Inf)
+      val move4 = Await.result(Future(simulateIteration(4, moves:::path)), Duration.Inf)
+      val move5 = Await.result(Future(simulateIteration(5, moves:::path)), Duration.Inf)
 
-  def simulateMove(fieldNumber: Int): Unit =
-    simulations(fieldNumber - 1) = game.copy()
-    simulations(fieldNumber - 1).move(fieldNumber-1, playerId)
-    decisions.getNode(moves).addNewKidAt(simulations(fieldNumber - 1).getScore(playerId), fieldNumber - 1)
+      if move0 == playerId then simulateMove(path:+0)
+      if move1 == playerId then simulateMove(path:+1)
+      if move2 == playerId then simulateMove(path:+2)
+      if move3 == playerId then simulateMove(path:+3)
+      if move4 == playerId then simulateMove(path:+4)
+      if move5 == playerId then simulateMove(path:+5)
+    simulateMove(List())
 
-  def addEnemyMove(move: Int): Unit = moves = moves :+ move
+
+  def simulateIteration(fieldNumber: Int, way: List[Int]): Int =
+    val curr = decisions.getNode(way)
+    val simulationGame = curr.getValue._2.copy()
+    val whoseNext = simulationGame.move(playerId, fieldNumber)
+    curr.addNewKidAt((simulationGame.getScore(playerId), simulationGame),fieldNumber)
+    whoseNext
+
 
 }
--- Tree.scala
+++ Tree.scala
@@ -9,34 +9,57 @@ class Node[T](var value: T, val size: Int) {
 
   def setValue(newValue: T): Unit = value = newValue
 
+  def hasKids: Boolean = index != 0
+
   def getKids: Array[Node[T]] = kids
+
+  def getIndex: Int = index
 
   def addKid(newKid: Node[T]): Unit =
     kids(index) = newKid
-    index += 1
+    if index == -1 then index = 1
+    else index += 1
 
   def addNewKid(value: T): Unit =
     kids(index) = new Node[T](value, kids.length)
+    if index == -1 then index = 1
+    else index += 1
 
   def addKidAt(newKid: Node[T], index: Int): Unit =
     kids(index) = newKid
+    if this.index == 0 then this.index = -1
 
   def addNewKidAt(value: T, index: Int): Unit =
     kids(index) = new Node[T](value, kids.length)
+    if this.index == 0 then this.index = -1
+
+  def findPaths(): Array[(T, List[Int])] =
+    var res = new Array[(T, List[Int])](0)
+    def search(curr: Node[T], path: List[Int]): Unit =
+      if curr.hasKids then
+        var i = 0
+        while(i < size){
+          res = res:+(curr.getKids(i).getValue, path:+i)
+          search(curr.getKids(i), path:+i)
+          i += 1
+        }
+    search(this,List())
+    res  
 }
 
-class Tree(val kidsLimit: Int){
+class MyTree(val kidsLimit: Int, game: Kalaha){
 
-  var root: Node[Int] = Node[Int](0, kidsLimit)
+  var root: Node[(Int, Kalaha)] = Node[(Int, Kalaha)]((0, game), kidsLimit)
 
-  def getNode(path: List[Int]): Node[Int] =
+  def getNode(path: List[Int]): Node[(Int, Kalaha)] =
     @tailrec
-    def iterate(curr: Node[Int], tail: List[Int]): Node[Int] =
-      tail match
-        case Nil => curr
-        case h::t => iterate(curr.getKids(h-1), t)
+    def iterate(curr: Node[(Int, Kalaha)], tail: List[Int]): Node[(Int, Kalaha)] =
+      if !curr.hasKids then curr
+      else tail match
+          case Nil => curr
+          case h::t => iterate(curr.getKids(h), t)
     iterate(root, path)
-
+  
 
 }
 
