f41d1d23c34ad88427a76b61091457f746741e58
ASerwicka <aleserwicka@gmail.com>
2022-01-17 14:50:52 +0100

    lab 8

---
+++ Main.scala
@@ -0,0 +1,13 @@
+package Project
+import java.util.Scanner
+import scala.collection.mutable.Map
+import scala.util.Random
+
+object Main {
+
+  def main(args: Array[String]) : Unit = {
+    val server = new Server
+    server.chooseGame()
+  }
+
+}
