c49ab62bd2c3a44c84a7edae781c8e3dd2042320
mmiKky <michal.wojcieszak.dev@gmail.com>
2022-01-23 15:42:21 +0100

    add human player

--- kalaha/players/HumanPlayer.scala
+++ kalaha/players/HumanPlayer.scala
@@ -6,4 +6,16 @@ import scala.io.StdIn.readInt
 
 class HumanPlayer(id: Int) extends Player(id):
 
-  def makeMove(): Int = 0
+  def makeMove(): Int =
+    readUserChoice()
+
+  private def readUserChoice(): Int =
+    var userChoice = -1
+
+    print("Player " + id + ": Please enter number representing house(1-6): ")
+    try{
+      userChoice = readInt()
+    }catch{
+      case _ : Throwable => println("That's not an integer number")
+    }
+    userChoice
